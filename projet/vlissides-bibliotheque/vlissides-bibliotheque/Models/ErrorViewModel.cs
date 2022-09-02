2392dfa5c33f715aa1c5141c84761890d088f365
SaMongeon <samongeon@outlook.fr>
2022-09-02 09:24:01 -0400

    Génération de base du projet MVC
    Générer le projet .Net Core sur Visual Studio avec les
    dépendances de bases relatives à framework core.

---
+++ projet/vlissides-bibliotheque/vlissides-bibliotheque/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace vlissides_bibliotheque.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
