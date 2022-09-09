a898cf5ba2905a8f701c3261562ec37b68ef7306
SaMongeon <samongeon@outlook.fr>
2022-09-09 10:37:47 -0400

    docs: commenter classes et méthodes de connexion
    Ajout des commentaires pour les classes et les méthodes du controller
    de connexion.

--- projet/vlissides-bibliotheque/vlissides-bibliotheque/Controllers/ConnexionController.cs
+++ projet/vlissides-bibliotheque/vlissides-bibliotheque/Controllers/ConnexionController.cs
@@ -2,18 +2,34 @@
 
 namespace vlissides_bibliotheque.Controllers
 {
+    /// <summary>
+    /// Classe <c>ConnexionController</c> gère les url(s) pour les pages
+    /// relatives à la connexion d'un utilisateur.
+    /// </summary>
     public class ConnexionController : Controller
     {
+        /// <summary>
+        /// Retourne la page de connexion pour un utilisateur.
+        /// </summary>
+        /// <returns>Page de connexion.</returns>
         public IActionResult Index()
         {
             return View();
         }
 
+        /// <summary>
+        /// Retourne la page de création d'un nouvel utilisateur.
+        /// </summary>
+        /// <returns>Page d'inscription.</returns>
         public IActionResult Inscription()
         {
             return View();
         }
 
+        /// <summary>
+        /// Retourne la page de déconnexion pour un utilisateur.
+        /// </summary>
+        /// <returns>Page d'accueil.</returns>
         public IActionResult Logout()
         {
             return View();
