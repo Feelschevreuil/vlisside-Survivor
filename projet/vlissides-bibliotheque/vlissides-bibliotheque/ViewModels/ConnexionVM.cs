e335ae41ad8d60f5b55dc0a5ed976d8681c08b26
SaMongeon <samongeon@outlook.fr>
2022-09-09 11:36:55 -0400

    feat: ajout des attributs de ConnexionVM
    Ajouter les paramètres nécessairent à la connexion d'un utilisateur
    dans le ViewModel ConnexionVM.

--- projet/vlissides-bibliotheque/vlissides-bibliotheque/ViewModels/ConnexionVM.cs
+++ projet/vlissides-bibliotheque/vlissides-bibliotheque/ViewModels/ConnexionVM.cs
@@ -1,6 +1,20 @@
-﻿namespace vlissides_bibliotheque.ViewModels
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace vlissides_bibliotheque.ViewModels
 {
     public class ConnexionVM
     {
+        [Required]
+        [Display(Name = "Courriel")]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        [Display(Name = "Mot de passe")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [Display(Name = "Se souvenir de moi?")]
+        public bool RememberMe { get; set; }
     }
 }
