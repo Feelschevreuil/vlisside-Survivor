55521748bfa8c9f0d036381418994d5d05bd221b
Samue Mongeon <2036516@cegepgranby.qc.ca>
2022-09-12 15:54:22 -0400

    feat: ajout de la logique de connexion
    Ajout de la logique de connexion dans le Controlleur Connexion pour la
    page Index.

--- projet/vlissides-bibliotheque/vlissides-bibliotheque/Controllers/ConnexionController.cs
+++ projet/vlissides-bibliotheque/vlissides-bibliotheque/Controllers/ConnexionController.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using vlissides_bibliotheque.ViewModels;
 
 namespace vlissides_bibliotheque.Controllers
 {
@@ -8,13 +11,38 @@ namespace vlissides_bibliotheque.Controllers
     /// </summary>
     public class ConnexionController : Controller
     {
+        private readonly SignInManager<IdentityUser> _signInManager;
+
+        public ConnexionController(SignInManager<IdentityUser> signInManager)
+        {
+            _signInManager = signInManager;
+        }
+
         /// <summary>
         /// Retourne la page de connexion pour un utilisateur.
         /// </summary>
         /// <returns>Page de connexion.</returns>
+        [HttpGet]
         public IActionResult Index()
         {
             return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> IndexAsync(ConnexionVM vm)
+        {
+            if (ModelState.IsValid) {
+                var result = await _signInManager.PasswordSignInAsync(vm.Email, vm.Password, vm.RememberMe, lockoutOnFailure: false);
+                if (result.Succeeded) {
+                    return View();
+                }
+                if (!result.IsLockedOut) {
+                    ModelState.AddModelError(string.Empty, "Tentative de connexion invalide.");
+                    return View(vm);
+                }
+            }
+
+            return View(vm);
         }
 
         /// <summary>
