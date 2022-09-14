6f67cdcc06a00f506f052ddae54b091981450fea
SAMORY ROY <1855390@CegepGranby.qc.ca>
2022-09-14 09:12:07 -0400

    fix: ajout de la classe recommendationpromotionsvm
    ajout et modification de la classe viewmodel RecommendationPromotionsVM
    dans l'accueil

---
+++ projet/vlissides-bibliotheque/vlissides-bibliotheque/ViewModels/RecommendationPromotionsVM.cs
@@ -0,0 +1,10 @@
+﻿using vlissides_bibliotheque.Models;
+
+namespace vlissides_bibliotheque.ViewModels
+{
+    public class RecommendationPromotions
+    {
+        List<LivreBibliotheque>? livreBibliotheques;
+        List<Commanditaire>? commanditaires;
+    }
+}
