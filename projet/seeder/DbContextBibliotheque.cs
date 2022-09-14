8c34e1f74e1c56477120e7adb7242e79214f04a7
rob <2060115@cegepgranby.qc.ca>
2022-09-14 07:35:01 -0400

    feat: ajout du dbcontext

---
+++ projet/seeder/DbContextBibliotheque.cs
@@ -0,0 +1,24 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using System.IO;
+using vlissides-bibliothque.Data;
+
+namespace seeder {
+	class DbContextBibliotheque {
+
+		public static ApplicationDbContext CreateDbContext() {
+
+			IConfigurationRoot configuration = new ConfigurationBuilder()
+				.SetBasePath(Directory.GetCurrentDirectory())
+				.AddJsonFile(@Directory.GetCurrentDirectory() + "/appsettings.json")
+				.Build();
+
+			var builder = new DbContextOptionsBuilder<ApplicationDbContext>();
+
+			// mssql
+			var connectionString = configuration.getConnectionString("mssql");
+			builder.UseSqlServer(connectionString);
+		}
+	}
+}
+
