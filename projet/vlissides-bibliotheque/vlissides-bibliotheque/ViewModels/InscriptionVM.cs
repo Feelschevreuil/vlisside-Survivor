58ee39fa0b1873a7b94f9672eab6e0446a741392
Samue Mongeon <2036516@cegepgranby.qc.ca>
2022-09-14 12:13:33 -0400

    feat: ajout attributs pour le vm d'inscription.
    Ajout des attributs de base pour la création d'un nouvel utilisateur
    dans le fichier InscriptionVM.

--- projet/vlissides-bibliotheque/vlissides-bibliotheque/ViewModels/InscriptionVM.cs
+++ projet/vlissides-bibliotheque/vlissides-bibliotheque/ViewModels/InscriptionVM.cs
@@ -1,6 +1,52 @@
-﻿namespace vlissides_bibliotheque.ViewModels
+﻿using System.ComponentModel.DataAnnotations;
+using System.Xml.Linq;
+
+namespace vlissides_bibliotheque.ViewModels
 {
     public class InscriptionVM
     {
+        [Required(ErrorMessage = "Le champ {0} est requis.")]
+        [EmailAddress]
+        public string Courriel { get; set; }
+
+        [Required(ErrorMessage = "Le champ {0} est requis.")]
+        public string Nom { get; set; }
+
+        [Required(ErrorMessage = "Le champ {0} est requis.")]
+        [Display(Name = "Prénom")]
+        public string Prenom { get; set; }
+
+
+        [Required(ErrorMessage = "Le champ {0} est requis.")]
+        [Display(Name = "Mot de passe")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [Required(ErrorMessage = "Le champ {0} est requis.")]
+        [Display(Name = "Confirmer le mot de passe")]
+        [DataType(DataType.Password)]
+        public string ConfirmPassword { get; set; }
+
+        //[Required(ErrorMessage = "Le champ {0} est requis.")]
+        //[Display(Name = "Programme d'étude")]
+        //public ProgrammeEtude ProgrammeEtude { get; set; }
+
+        [Required(ErrorMessage = "Le champ {0} est requis.")]
+        [Display(Name = "Numéro civique")]
+        public string NoCivique { get; set; }
+
+        [Required(ErrorMessage = "Le champ {0} est requis.")]
+        public string Rue { get; set; }
+
+        [Required(ErrorMessage = "Le champ {0} est requis.")]
+        public string Ville { get; set; }
+
+        [Required(ErrorMessage = "Le champ {0} est requis.")]
+        [Display(Name = "Code postal")]
+        public string CodePostal { get; set; }
+
+        [Required(ErrorMessage = "Le champ {0} est requis.")]
+        [Display(Name = "No de téléphone")]
+        public string NoTelephone { get; set; }
     }
 }
