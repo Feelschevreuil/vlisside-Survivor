99af07f719095c609e70dd2e5eee2a7e15a886ab
rob <2060115@cegepgranby.qc.ca>
2022-09-14 17:29:31 -0400

    feat: ajout de la suppression des données de la bd

--- projet/seeder/Program.cs
+++ projet/seeder/Program.cs
@@ -13,6 +13,60 @@ namespace seeder {
 
 			context = DbContextBibliotheque.CreateDbContext();
 
+			enleverDonnees();
+		}
+
+		/// <summary>
+		/// Fonction qui enlève les données présentes de
+		/// la base de données.
+		/// </summary>
+		private static void enleverDonnees() {
+
+			context.Adresses
+				.RemoveRange(context.Adresses);
+
+			context.Auteurs
+				.RemoveRange(context.Auteurs);
+
+			context.AuteursLivres
+				.RemoveRange(context.AuteursLivres);
+
+			context.CommandesEtudiants
+				.RemoveRange(context.CommandesEtudiants);
+
+			context.Commanditaires
+				.RemoveRange(context.Commanditaires);
+
+			context.EtatsLivres
+				.RemoveRange(context.EtatsLivres);
+
+			context.Etudiants
+				.RemoveRange(context.Etudiants);
+
+			context.Evaluations
+				.RemoveRange(context.Evaluations);
+
+			context.EvaluationsLivres
+				.RemoveRange(context.EvaluationsLivres);
+
+			context.Evenements
+				.RemoveRange(context.Evenements);
+
+			context.LivresBibliotheques
+				.RemoveRange(context.LivresBibliotheques);
+
+			context.LivresEtudiants
+				.RemoveRange(context.LivresEtudiants);
+
+			context.ProgrammesEtudes
+				.RemoveRange(context.ProgrammesEtudes);
+
+			context.Utilisateurs
+				.RemoveRange(context.Utilisateurs);
+
+			context.TypesPaiements
+				.RemoveRange(context.TypesPaiements);
+
 		}
 	}
 }
