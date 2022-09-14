aadff7fb5eeba7d2993dcafe4c51ba42e6a95472
rob <2060115@cegepgranby.qc.ca>
2022-09-14 07:36:04 -0400

    feat: récupération du dbcontext au seeder

--- projet/seeder/Program.cs
+++ projet/seeder/Program.cs
@@ -1,2 +1,6 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using FizzWare.NBuilder;
+using vlissides-bibliotheque.Models;
+
+// Va chercher le dbcontext
+using var context = DbContextBibliotheque.CreateDbContext();
