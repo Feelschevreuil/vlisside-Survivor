53a6f5c7cd175664c3933586b454f6a85a4d8180
Samue Mongeon <2036516@cegepgranby.qc.ca>
2022-09-15 09:07:45 -0400

    fix: transformation des espaces en tab.
    Transformer les espaces ajoutés par mégardes en tabulations (options
    "tabify" offerte par VS).

--- projet/vlissides-bibliotheque/vlissides-bibliotheque/Validation/IsbnAttribute.cs
+++ projet/vlissides-bibliotheque/vlissides-bibliotheque/Validation/IsbnAttribute.cs
@@ -2,19 +2,19 @@
 
 namespace vlissides_bibliotheque.Validation
 {
-    public class IsbnAttribute : ValidationAttribute
-    {
+	public class IsbnAttribute : ValidationAttribute
+	{
 	// TODO: commenter
-        protected override ValidationResult? IsValid(object? value, ValidationContext validationContext)
-        {
-            string ISBN = (string)value;
+		protected override ValidationResult? IsValid(object? value, ValidationContext validationContext)
+		{
+			string ISBN = (string)value;
 
-            if(ISBN.Length == 10 || ISBN.Length == 13) 
-            {
-                return new ValidationResult(ErrorMessage = $"Le nombre de charactères de l'ISBN doit être entre 10 et 13.");
-            }
+			if(ISBN.Length == 10 || ISBN.Length == 13) 
+			{
+				return new ValidationResult(ErrorMessage = $"Le nombre de charactères de l'ISBN doit être entre 10 et 13.");
+			}
 
-            return ValidationResult.Success;
-        }
-    }
+			return ValidationResult.Success;
+		}
+	}
 }
