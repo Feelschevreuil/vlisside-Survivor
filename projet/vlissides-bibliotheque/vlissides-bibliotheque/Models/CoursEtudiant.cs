ca629f4e057cc5fc510c4ca854e68d512351c840
Samue Mongeon <2036516@cegepgranby.qc.ca>
2022-09-19 13:34:56 -0400

    feat: ajout classe CoursEtudiant
    Ajout d'une table de liaison entre les étudiants et les cours.

---
+++ projet/vlissides-bibliotheque/vlissides-bibliotheque/Models/CoursEtudiant.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace vlissides_bibliotheque.Models
+{
+    public class CoursEtudiant
+    {
+        [Required]
+        public int CoursId { get; set; }
+        [Required]
+        public Cours Cours { get; set; }
+
+        [Required]
+        public int EtudiantId { get; set; }
+        [Required]
+        public Etudiant Etudiant { get; set; }
+    }
+}
