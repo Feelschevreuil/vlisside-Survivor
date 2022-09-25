d21bc71b7ec3be9add93a1e9820393050c6b756b
rob <2060115@cegepgranby.qc.ca>
2022-09-25 14:58:32 +0000

    tâche(mssql): nouveau nom de connection string

--- projet/vlissides-bibliotheque/vlissides-bibliotheque/Program.cs
+++ projet/vlissides-bibliotheque/vlissides-bibliotheque/Program.cs
@@ -12,7 +12,7 @@ namespace vlissides_bibliotheque
             var builder = WebApplication.CreateBuilder(args);
 
             // Add services to the container.
-            var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
+            var connectionString = builder.Configuration.GetConnectionString("mssql") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
             builder.Services.AddDbContext<ApplicationDbContext>(options =>
                 options.UseSqlServer(connectionString));
             builder.Services.AddDatabaseDeveloperPageExceptionFilter();
