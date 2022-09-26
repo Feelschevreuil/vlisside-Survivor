293f523f047dd253da9d63d4e9afbca2a250386a
Samue Mongeon <2036516@cegepgranby.qc.ca>
2022-09-26 16:43:12 -0400

    feat: extension pour utilisateur
    Ajout d'une extension pour les utilisateurs (plus précisément pour les
    étudiants) pour récupérer l'adresse de livraison et l'adresse de
    facturation de celui-ci.

---
+++ projet/vlissides-bibliotheque/vlissides-bibliotheque/Extensions/UtilisateurExtension.cs
@@ -0,0 +1,24 @@
+﻿using System.Net.NetworkInformation;
+using vlissides_bibliotheque.Models;
+
+namespace vlissides_bibliotheque.Extensions
+{
+    public static class UtilisateurExtension
+    {
+        public static Etudiant GetAdresseLivraison(this Etudiant etudiant)
+        {
+            int adresseId = etudiant.AdresseLivraisonId;
+
+
+
+            return 
+        }
+
+        public static Etudiant GetAdresseFacturation(this Etudiant etudiant)
+        {
+            int adresseId = etudiant.AdresseFacturationId;
+
+            return
+        }
+    }
+}
