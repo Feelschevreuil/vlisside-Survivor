c46efd76bea18b654dc0c607bdd1f6822d2e1ae8
Samue Mongeon <2036516@cegepgranby.qc.ca>
2022-09-26 16:45:59 -0400

    feat: ajout du controlleur pour l'extension
    Ajout du controlleur pour l'extension d'utilisateur.

--- projet/vlissides-bibliotheque/vlissides-bibliotheque/Extensions/UtilisateurExtension.cs
+++ projet/vlissides-bibliotheque/vlissides-bibliotheque/Extensions/UtilisateurExtension.cs
@@ -1,10 +1,19 @@
-﻿using System.Net.NetworkInformation;
+﻿using Microsoft.AspNetCore.Identity;
+using System.Net.NetworkInformation;
+using vlissides_bibliotheque.Data;
 using vlissides_bibliotheque.Models;
 
 namespace vlissides_bibliotheque.Extensions
 {
     public static class UtilisateurExtension
     {
+        private readonly ApplicationDbContext _context;
+
+        public UtilisateurExtension(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
         public static Etudiant GetAdresseLivraison(this Etudiant etudiant)
         {
             int adresseId = etudiant.AdresseLivraisonId;
