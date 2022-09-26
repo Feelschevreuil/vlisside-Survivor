651c2e5859fdae5dcf7dd34d5452ae7c6b435f90
VINCENT CHOUINARD <1624901@CegepGranby.qc.ca>
2022-09-26 15:58:07 -0400

    docs: ajout du summery de RecommendationPromotionsVM

--- projet/vlissides-bibliotheque/vlissides-bibliotheque/ViewModels/RecommendationPromotionsVM.cs
+++ projet/vlissides-bibliotheque/vlissides-bibliotheque/ViewModels/RecommendationPromotionsVM.cs
@@ -2,6 +2,9 @@
 
 namespace vlissides_bibliotheque.ViewModels
 {
+    /// <summary>
+    /// Combine les classes « EvaluationLivre » et la classe « Evenement » en liste distinct pour les affichers dans la page d'accueil
+    /// </summary>
     public class RecommendationPromotionsVM
     {
         public List<EvaluationLivre> livreBibliothequesEvaluation;
