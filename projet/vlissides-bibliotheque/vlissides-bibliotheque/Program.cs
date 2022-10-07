7e78629fa4c3537046113eefefb76ecaa0346688
VINCENT CHOUINARD <1624901@CegepGranby.qc.ca>
2022-10-07 08:29:25 -0400

    style: Tabify Visuel studio trouvais que la page pouvais être tabify

--- projet/vlissides-bibliotheque/vlissides-bibliotheque/Program.cs
+++ projet/vlissides-bibliotheque/vlissides-bibliotheque/Program.cs
@@ -6,58 +6,58 @@ using System.Runtime.InteropServices;
 
 namespace vlissides_bibliotheque
 {
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            var builder = WebApplication.CreateBuilder(args);
+	public class Program
+	{
+		public static void Main(string[] args)
+		{
+			var builder = WebApplication.CreateBuilder(args);
 
-	    string connectionString;
-	    connectionString = builder.Configuration.GetConnectionString("mssql") ?? throw new InvalidOperationException("Connection string 'mssql' not found.");
-    
-	    builder.Services.AddDbContext<ApplicationDbContext>(options =>
+		string connectionString;
+		connectionString = builder.Configuration.GetConnectionString("mssql") ?? throw new InvalidOperationException("Connection string 'mssql' not found.");
+	
+		builder.Services.AddDbContext<ApplicationDbContext>(options =>
 		options.UseSqlServer(connectionString)
-	    );
+		);
 
-            builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+			builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
-            builder.Services.AddDefaultIdentity<Utilisateur>(options => options.SignIn.RequireConfirmedAccount = true)
-                .AddRoles<IdentityRole>()
-                .AddSignInManager()
-                .AddEntityFrameworkStores<ApplicationDbContext>();
+			builder.Services.AddDefaultIdentity<Utilisateur>(options => options.SignIn.RequireConfirmedAccount = true)
+				.AddRoles<IdentityRole>()
+				.AddSignInManager()
+				.AddEntityFrameworkStores<ApplicationDbContext>();
 
-            builder.Services.AddIdentityCore<Etudiant>()
-                .AddRoles<IdentityRole>()
-                .AddSignInManager()
-                .AddEntityFrameworkStores<ApplicationDbContext>();
+			builder.Services.AddIdentityCore<Etudiant>()
+				.AddRoles<IdentityRole>()
+				.AddSignInManager()
+				.AddEntityFrameworkStores<ApplicationDbContext>();
 
-            builder.Services.AddControllersWithViews();
+			builder.Services.AddControllersWithViews();
 
-            var app = builder.Build();
+			var app = builder.Build();
 
-            // Configure the HTTP request pipeline.
-            if (app.Environment.IsDevelopment()) {
-                app.UseMigrationsEndPoint();
-            } else {
-                app.UseExceptionHandler("/Home/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
-            }
+			// Configure the HTTP request pipeline.
+			if (app.Environment.IsDevelopment()) {
+				app.UseMigrationsEndPoint();
+			} else {
+				app.UseExceptionHandler("/Home/Error");
+				// The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+				app.UseHsts();
+			}
 
-            app.UseHttpsRedirection();
-            app.UseStaticFiles();
+			app.UseHttpsRedirection();
+			app.UseStaticFiles();
 
-            app.UseRouting();
+			app.UseRouting();
 
-            app.UseAuthentication();
-            app.UseAuthorization();
+			app.UseAuthentication();
+			app.UseAuthorization();
 
-            app.MapControllerRoute(
-                name: "default",
-                pattern: "{controller=Home}/{action=Index}/{id?}");
-            app.MapRazorPages();
+			app.MapControllerRoute(
+				name: "default",
+				pattern: "{controller=Home}/{action=Index}/{id?}");
+			app.MapRazorPages();
 
-            app.Run();
-        }
-    }
+			app.Run();
+		}
+	}
 }
