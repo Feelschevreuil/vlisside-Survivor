39e57f28aa1c386e4d2dbc01e4834e3bc6b2156a
rob <2060115@cegepgranby.qc.ca>
2022-10-11 12:43:00 -0400

    tâche: ajout de la migration sqlite

--- projet/vlissides-bibliotheque/vlissides-bibliotheque/Data/Migrations/sqlite/20221011163948_Initial.Designer.cs
+++ projet/vlissides-bibliotheque/vlissides-bibliotheque/Data/Migrations/sqlite/20221011163948_Initial.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using vlissides_bibliotheque.Data;
 
@@ -10,9 +11,10 @@ using vlissides_bibliotheque.Data;
 namespace vlissides_bibliotheque.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20221011163948_Initial")]
+    partial class Initial
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "6.0.9");
