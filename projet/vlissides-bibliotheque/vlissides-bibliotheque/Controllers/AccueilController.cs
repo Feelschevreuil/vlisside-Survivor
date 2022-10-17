51b15906af06ec0825533205b22e4aa3e718a3d9
VINCENT CHOUINARD <1624901@CegepGranby.qc.ca>
2022-10-17 16:37:42 -0400

    test: programme etude

--- projet/vlissides-bibliotheque/vlissides-bibliotheque/Controllers/AccueilController.cs
+++ projet/vlissides-bibliotheque/vlissides-bibliotheque/Controllers/AccueilController.cs
@@ -69,6 +69,30 @@ namespace vlissides_bibliotheque.Controllers
             List<LivreBibliotheque> listLivreBibliotheque = _context.LivresBibliotheque.ToList();
             Random random = new Random();
 
+
+            ProgrammeEtude programme = new()
+            {
+                Nom = "Fleur",
+                ProgrammeEtudeId = 0,
+                Code = "hfde",
+            };
+            _context.ProgrammesEtudes.Add(programme);
+            _context.SaveChanges();
+
+            Cours cours = new()
+            {
+                AnneeParcours = 1,
+                Nom = "Sicen",
+                Code = "45ksup",
+                CoursId = 0,
+                Description = "cours sur la modialisation",
+                ProgrammeEtudeId = _context.ProgrammesEtudes.First().ProgrammeEtudeId,
+            };
+            _context.Cours.Add(cours);
+            _context.SaveChanges();
+
+
+
             MaisonEdition maison = new()
             {
                 MaisonEditionId = 0,
@@ -144,26 +168,6 @@ namespace vlissides_bibliotheque.Controllers
                 //Modifer le code ci-dessus quand le seeder remplira toutes la BD
                 //Un associer doit être fait ici avec le Id.
                 //Ex: Cherche coursId pour avoir la variable cours remplie avec un object "cours" avant de l'envoyer à la vue 
-
-                ProgrammeEtude programme = new()
-                {
-                   Nom ="Fleur",
-                   ProgrammeEtudeId = 0,
-                   Code = "hfde",
-                };
-                _context.ProgrammesEtudes.Add(programme);
-                _context.SaveChanges();
-
-                Cours cours = new() {
-                    AnneeParcours = 1,
-                    Nom = "Sicen",
-                    Code = "45ksup",
-                    CoursId = 0,
-                    Description ="cours sur la modialisation",
-                    ProgrammeEtudeId = _context.ProgrammesEtudes.First().ProgrammeEtudeId,
-                };
-                _context.Cours.Add(cours);
-                _context.SaveChanges();
 
 
                 if (CoursLivre == null)
