3c9db6c1017e768ce12099d0a150f5e585716cdf
SAMORY ROY <1855390@CegepGranby.qc.ca>
2022-10-21 08:39:08 -0400

    feat: ajout des fichiers de constantes

---
+++ projet/vlissides-bibliotheque/vlissides-bibliotheque/Constantes/RolesName/NomEtatLivre.cs
@@ -0,0 +1,9 @@
+﻿namespace vlissides_bibliotheque.Constantes.RolesName
+{
+    public class NomEtatLivre
+    {
+        public const string Neuf = "Neuf";
+        public const string Numerique = "Numérique";
+        public const string Usagee = "Usagé";
+    }
+}
