461ace1c2eb37a6e12cc1f281492f2087d5c01a0
SAMORY ROY <1855390@CegepGranby.qc.ca>
2022-10-21 08:39:08 -0400

    feat: ajout des fichiers de constantes

---
+++ projet/vlissides-bibliotheque/vlissides-bibliotheque/Constantes/RolesName/Emails.cs
@@ -0,0 +1,7 @@
+﻿namespace vlissides_bibliotheque.Constantes.RolesName
+{
+    public class Emails
+    {
+        public const string EmailAdmin = "admin@cegep-connaissance-aleatoire.qc.ca";
+    }
+}
---
+++ projet/vlissides-bibliotheque/vlissides-bibliotheque/Constantes/RolesName/RolesName.cs
@@ -0,0 +1,9 @@
+﻿namespace vlissides_bibliotheque.Constantes.Roles
+{
+    public class RolesName
+    {
+        public const string Admin = "Admin";
+        public const string Utilisateur = "Utilisateur";
+        public const string Etudiant = "Etudiant";
+    }
+}
