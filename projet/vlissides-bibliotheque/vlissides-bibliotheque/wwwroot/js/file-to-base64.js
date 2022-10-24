5c4cf8557b5c2e5458651eaaca80230763167ede
Samuel Mongeon <2036516@cegepgranby.qc.ca>
2022-10-24 14:07:04 -0400

    doc: ajout de commentaires
    Ajout de commentaires pour les fonctions javascript en suivant les
    standards prévus à cet effet.

--- projet/vlissides-bibliotheque/vlissides-bibliotheque/wwwroot/js/file-to-base64.js
+++ projet/vlissides-bibliotheque/vlissides-bibliotheque/wwwroot/js/file-to-base64.js
@@ -1,4 +1,7 @@
-﻿
+﻿/// <summary>
+/// Convertit un fichier en base 64.
+/// </summary>
+/// <returns>Object Promise avec fichier en base 64</returns>
 function toBase64(file) {
     return new Promise((resolve) => {
         const reader = new FileReader();
@@ -7,6 +10,10 @@ function toBase64(file) {
     });
 }
 
+/// <summary>
+/// Récupère un fichier en base 64 et l'affiche.
+/// </summary>
+/// <returns></returns>
 function getAndShowBase64(file, senderId, viewerId) {
     if (file != undefined && typeof file == "object") {
         toBase64(file).then(function (res) {
