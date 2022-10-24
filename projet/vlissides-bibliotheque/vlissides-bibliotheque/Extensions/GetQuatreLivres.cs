bf28256a3785f9ff185d0f8fbdc6a278f0d230e8
VINCENT CHOUINARD <1624901@CegepGranby.qc.ca>
2022-10-24 15:40:12 -0400

    feat: nouvelle extention pour l'affichage

---
+++ projet/vlissides-bibliotheque/vlissides-bibliotheque/Extensions/GetQuatreLivres.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using System.Net.NetworkInformation;
+using vlissides_bibliotheque.Data;
+using vlissides_bibliotheque.Models;
+using vlissides_bibliotheque.ViewModels;
+
+namespace vlissides_bibliotheque
+{
+    public static class GetQuatreLivres
+    {
+        public static List<TuileLivreBibliotequeVM> GetInventaireBibliotequeVMs(ApplicationDbContext _context)
+        {
+            List<TuileLivreBibliotequeVM> listTuileLivreBibliotequeVMs = new();
+            IEnumerable<LivreBibliotheque> listQuatreLivre = _context.LivresBibliotheque.Take(4);
+
+            foreach (LivreBibliotheque livre in listQuatreLivre)
+            {
+                var livreConvertie = livre.GetTuileLivreBibliotequeVMs(_context);
+                listTuileLivreBibliotequeVMs.Add(livreConvertie);
+            };
+
+            return listTuileLivreBibliotequeVMs;
+        }
+    }
+}
