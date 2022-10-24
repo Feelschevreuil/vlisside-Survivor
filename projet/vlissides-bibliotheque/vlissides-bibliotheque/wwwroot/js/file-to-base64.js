adfd4cb92e2d7df0e541980fc6d475005e3a2b0a
Samuel Mongeon <2036516@cegepgranby.qc.ca>
2022-10-24 13:44:14 -0400

    feat: ajout code de base convertion base64
    Ajout des fonctions de base pour convetir un fichier en base 64.

--- projet/vlissides-bibliotheque/vlissides-bibliotheque/wwwroot/js/file-to-base64.js
+++ projet/vlissides-bibliotheque/vlissides-bibliotheque/wwwroot/js/file-to-base64.js
@@ -1,1 +1,13 @@
 ﻿
+const toBase64 = file => new Promise((resolve) => {
+    const reader = new FileReader();
+    reader.readAsDataURL(file);
+    reader.onload = () => resolve(reader.result);
+});
+
+function convertFileToBase64(file, senderId, viewerId) {
+    toBase64(file).then(function (res) {
+        document.getElementById(senderId).value = res;
+        document.getElementById(viewerId).src = res;
+    });
+}
