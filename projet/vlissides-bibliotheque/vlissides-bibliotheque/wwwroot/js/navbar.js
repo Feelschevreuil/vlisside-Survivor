5daeef979c142e48adb45d8a8e8af9f37c573d41
Samuel Mongeon <2036516@cegepgranby.qc.ca>
2022-10-27 11:38:00 -0400

    feat: ajout du code js pour gérer la navbar
    Ajout de code pour enlever ou ajouter des éléments dans la barre de
    navigation.

--- projet/vlissides-bibliotheque/vlissides-bibliotheque/wwwroot/js/navbar.js
+++ projet/vlissides-bibliotheque/vlissides-bibliotheque/wwwroot/js/navbar.js
@@ -1,1 +1,32 @@
-﻿
+﻿window.addEventListener("resize", () => {
+    setBottomNavbar();
+});
+
+function setBottomNavbar() {
+    let screenWidth = window.innerWidth;
+    let bottomNav = document.querySelector("#bottomNav");
+    let copyrightInfo = bottomNav.querySelector("#copyrightInfo");
+    let bottomNavContent = document.querySelector("#bottomNavContent");
+    let livreLinks = document.querySelectorAll("#livreLink");
+    console.log(livreLinks);
+
+    if (screenWidth <= 575) {
+
+        bottomNav.classList.replace("position-absolute", "position-fixed");
+        copyrightInfo.classList.replace("d-block", "d-none");
+        bottomNavContent.classList.replace("d-none", "d-block");
+        for (let livreLink of livreLinks) {
+            livreLink.classList.add("d-none");
+        }
+    } else {
+
+        bottomNav.classList.replace("position-fixed", "position-absolute");
+        copyrightInfo.classList.replace("d-none", "d-block");
+        bottomNavContent.classList.replace("d-block", "d-none");
+        for (let livreLink of livreLinks) {
+            livreLink.classList.remove("d-none");
+        }
+    }
+}
+
+setBottomNavbar();
