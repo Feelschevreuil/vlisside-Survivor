96b912250c3c928ab0ddea4614f704b23037d295
Samuel Mongeon <2036516@cegepgranby.qc.ca>
2022-10-28 12:43:22 -0400

    feat: ajout du code js de gestion de téléphone
    Ajout du code js qui formate le numéro de téléphone automatiquement.

--- projet/vlissides-bibliotheque/vlissides-bibliotheque/wwwroot/js/froms.js
+++ projet/vlissides-bibliotheque/vlissides-bibliotheque/wwwroot/js/froms.js
@@ -1,5 +1,39 @@
-﻿function formatPhone(phoneNumber) {
+﻿function formatPhone(fakePhoneNumberInput, phoneNumberInput) {
+    if(fakePhoneNumberInput != undefined && phoneNumberInput != undefined) {
+        let phoneNumber = fakePhoneNumberInput.value;
+
+        // remplacer tous les caractères qui ne sont pas des nombres
+        phoneNumber = phoneNumber.replace(/\D/g,"");
     
+        phoneNumberInput.value = parseInt(phoneNumber);
+    
+        let formattedPhoneNumber = phoneNumber
+    
+        if(phoneNumber.length < 3) {
+            formattedPhoneNumber = `(${formattedPhoneNumber})`;
+        }
+        if(phoneNumber.length >= 3) {
+            // format : (666) ***
+            let tempFormattedPhoneNumber = `(${formattedPhoneNumber[0]}${formattedPhoneNumber[1]}${formattedPhoneNumber[2]}) `;
+            // ajout du reste
+            for(let i = 3; i < formattedPhoneNumber.length; i++) {
+                tempFormattedPhoneNumber += formattedPhoneNumber;
+            }
+            formattedPhoneNumber = tempFormattedPhoneNumber;
+        }
+        if(phoneNumber.length >= 6) {
+            // format : (666) 666-****
+            let tempFormattedPhoneNumber = `(${formattedPhoneNumber[0]}${formattedPhoneNumber[1]}${formattedPhoneNumber[2]}) `;
+                                        + `${formattedPhoneNumber[3]}${formattedPhoneNumber[4]}${formattedPhoneNumber[5]}-`;
+            // ajout du reste
+            for(let i = 3; i < formattedPhoneNumber.length; i++) {
+                tempFormattedPhoneNumber += formattedPhoneNumber;
+            }
+            formattedPhoneNumber = tempFormattedPhoneNumber;
+        }
+    
+        fakePhoneNumberInput.value =  formattedPhoneNumber;
+    }
 }
 
 function formatPostalCode(postalCode) {
