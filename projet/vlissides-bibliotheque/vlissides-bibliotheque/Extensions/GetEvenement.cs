82fee588bdf113a6358798157474caf1853047a0
VINCENT CHOUINARD <1624901@CegepGranby.qc.ca>
2022-10-31 16:00:51 -0400

    feat: extention pour transformer les evenements en VM

---
+++ projet/vlissides-bibliotheque/vlissides-bibliotheque/Extensions/GetEvenement.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using System.Net.NetworkInformation;
+using vlissides_bibliotheque.Data;
+using vlissides_bibliotheque.Models;
+using vlissides_bibliotheque.ViewModels;
+
+namespace vlissides_bibliotheque
+{
+    public static class GetEvenement
+    {
+        public static List<EvenementVM> GetEvenements(List<Evenement> listEvenements)
+        {
+            List<EvenementVM> listEvenementsVM = new();
+            IEnumerable<Evenement> listQuatreEvenement = listEvenements;
+
+            foreach (Evenement evenement in listQuatreEvenement)
+            {
+                EvenementVM evenementVM = new()
+                {
+                    EvenementId = evenement.EvenementId,
+                    Commanditaire = evenement.Commanditaire,
+                    CommanditaireId = evenement.CommanditaireId,
+                    Debut = evenement.Debut,
+                    Fin = evenement.Fin,
+                    Image = evenement.Image,
+                    Nom =evenement.Nom,
+                    Description = evenement.Description,
+                };
+                listEvenementsVM.Add(evenementVM);
+            };
+
+            return listEvenementsVM;
+        }
+    }
+}
---
+++ projet/vlissides-bibliotheque/vlissides-bibliotheque/ViewModels/EvenementVM.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel.DataAnnotations;
+using vlissides_bibliotheque.Models;
+
+namespace vlissides_bibliotheque.ViewModels
+{
+    public class EvenementVM
+    {
+        [Required]
+        public int EvenementId { get; set; }
+
+        [Required]
+        public int CommanditaireId { get; set; }
+        public Commanditaire Commanditaire { get; set; }
+
+        [Required]
+        [StringLength(64)]
+        public string Nom { get; set; }
+
+        [Required]
+        public DateTime Debut { get; set; }
+
+        [Required]
+        public DateTime Fin { get; set; }
+
+        [Required]
+        [StringLength(512)]
+        public string Description { get; set; }
+
+        public string Image { get; set; }
+    }
+}
