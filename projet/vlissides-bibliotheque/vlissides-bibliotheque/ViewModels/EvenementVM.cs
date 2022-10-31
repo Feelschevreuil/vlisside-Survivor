1bdc8da13b4543048838d975a47deaef54e0fe5d
VINCENT CHOUINARD <1624901@CegepGranby.qc.ca>
2022-10-31 17:13:20 -0400

    feat: ajoute des messages d'erreurs en français

--- projet/vlissides-bibliotheque/vlissides-bibliotheque/ViewModels/EvenementVM.cs
+++ projet/vlissides-bibliotheque/vlissides-bibliotheque/ViewModels/EvenementVM.cs
@@ -5,27 +5,30 @@ namespace vlissides_bibliotheque.ViewModels
 {
     public class EvenementVM
     {
-        [Required]
+        [Required(ErrorMessage = "Le champ {0} est requis.")]
         public int EvenementId { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = "Le champ {0} est requis.")]
         public int CommanditaireId { get; set; }
+        [Display(Name = "Commanditaire")]
         public Commanditaire Commanditaire { get; set; }
 
-        [Required]
-        [StringLength(64)]
+        [Required(ErrorMessage = "Le champ {0} est requis.")]
+        [StringLength(64, ErrorMessage ="Votre {0} est trop longue")]
         public string Nom { get; set; }
 
-        [Required]
-        public DateTime Debut { get; set; }
+        [Required(ErrorMessage = "Le champ {0} est requis.")]
+        [DataType(DataType.Date)]
+        public DateTime Debut { get; set; } = DateTime.Now;
 
-        [Required]
-        public DateTime Fin { get; set; }
+        [Required(ErrorMessage = "Le champ {0} est requis.")]
+        [DataType(DataType.Date)]
+        public DateTime Fin { get; set; } = DateTime.Now;
 
-        [Required]
-        [StringLength(512)]
+        [Required(ErrorMessage = "Le champ {0} est requis.")]
+        [StringLength(512, ErrorMessage ="Votre {0} est trop longue")]
         public string Description { get; set; }
-
+        [Required(ErrorMessage = "Le champ {0} est requis.")]
         public string Image { get; set; }
     }
 }
