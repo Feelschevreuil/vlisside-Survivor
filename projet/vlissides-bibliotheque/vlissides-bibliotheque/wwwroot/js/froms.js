92efac026f77cbdff38188188fe5ed58dd722c8a
Samuel Mongeon <2036516@cegepgranby.qc.ca>
2022-10-31 13:27:44 -0400

    fix: optimisation du code
    Optimiser le code pour éviter l'utilisation inutile de variable et
    afin d'être plus efficace.

--- projet/vlissides-bibliotheque/vlissides-bibliotheque/wwwroot/js/froms.js
+++ projet/vlissides-bibliotheque/vlissides-bibliotheque/wwwroot/js/froms.js
@@ -4,32 +4,39 @@
 
         // remplacer tous les caractères qui ne sont pas des nombres
         phoneNumber = phoneNumber.replace(/\D/g,"");
-    
-        document.getElementById(phoneNumberInputId).value = parseInt(phoneNumber);
-    
-        let formattedPhoneNumber = phoneNumber
-    
-        if(phoneNumber.length < 3) {
-            formattedPhoneNumber = `(${formattedPhoneNumber})`;
+
+        // enlever les chiffres de trop
+        let phoneNumberWithMaxLength = "";
+        for(let i = 0; i < phoneNumber.length; i++) {
+            if(i < 10) {
+                phoneNumberWithMaxLength += phoneNumber[i];
+            }
         }
-        if(phoneNumber.length >= 3) {
+        phoneNumber = phoneNumberWithMaxLength;
+    
+        // ajouter la valeur à l'input
+        document.querySelector("#" + phoneNumberInputId).value = phoneNumber;
+    
+        // début formatage
+        let formattedPhoneNumber = "";
+    
+        if(phoneNumber.length > 0 && phoneNumber.length < 3) {
+            formattedPhoneNumber = `(${phoneNumber})`;
+        } else if(phoneNumber.length >= 3 && phoneNumber.length < 6) {
             // format : (666) ***
-            let tempFormattedPhoneNumber = `(${formattedPhoneNumber[0]}${formattedPhoneNumber[1]}${formattedPhoneNumber[2]}) `;
+            formattedPhoneNumber = `(${phoneNumber[0]}${phoneNumber[1]}${phoneNumber[2]}) `;
             // ajout du reste
-            for(let i = 3; i < formattedPhoneNumber.length; i++) {
-                tempFormattedPhoneNumber += formattedPhoneNumber;
+            for(let i = 3; i < phoneNumber.length; i++) {
+                formattedPhoneNumber += phoneNumber[i];
             }
-            formattedPhoneNumber = tempFormattedPhoneNumber;
-        }
-        if(phoneNumber.length >= 6) {
+        } else if(phoneNumber.length >= 6) {
             // format : (666) 666-****
-            let tempFormattedPhoneNumber = `(${formattedPhoneNumber[0]}${formattedPhoneNumber[1]}${formattedPhoneNumber[2]}) `;
-                                        + `${formattedPhoneNumber[3]}${formattedPhoneNumber[4]}${formattedPhoneNumber[5]}-`;
+            formattedPhoneNumber = `(${phoneNumber[0]}${phoneNumber[1]}${phoneNumber[2]}) `
+                                + `${phoneNumber[3]}${phoneNumber[4]}${phoneNumber[5]}-`;
             // ajout du reste
-            for(let i = 3; i < formattedPhoneNumber.length; i++) {
-                tempFormattedPhoneNumber += formattedPhoneNumber;
+            for(let i = 6; i < phoneNumber.length; i++) {
+                formattedPhoneNumber += phoneNumber[i];
             }
-            formattedPhoneNumber = tempFormattedPhoneNumber;
         }
     
         fakePhoneNumberInput.value =  formattedPhoneNumber;
