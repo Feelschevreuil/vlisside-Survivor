913b84fe7f8eede58ec3b383c650c0cf3e2bab06
Samuel Mongeon <2036516@cegepgranby.qc.ca>
2022-11-01 12:26:38 -0400

    feat: organisation du code pour être plus lisible
    Séparer le code js en plusieurs fonctions. Cela rend le tout plus
    lisible en ayant un code plus découpé et permettra d'effectué plus
    d'appels différents.

--- projet/vlissides-bibliotheque/vlissides-bibliotheque/wwwroot/js/froms.js
+++ projet/vlissides-bibliotheque/vlissides-bibliotheque/wwwroot/js/froms.js
@@ -7,9 +7,9 @@ document.addEventListener("keyup", (event) => {
     backspaceIsActive = event.key == "Backspace";
 });
 
-function formatPhone(fakePhoneNumberInput, phoneNumberInputId) {
-    if(fakePhoneNumberInput != undefined && phoneNumberInputId != undefined) {
-        let phoneNumber = fakePhoneNumberInput.value;
+function setPhoneNumber(valueContainerInput, rawDataInputId, formattedDataInputId) {
+    if(valueContainerInput != undefined) {
+        let phoneNumber = valueContainerInput.value;
 
         // supprimer un chiffre si backspace est appuyé
         if(backspaceIsActive) {
@@ -20,55 +20,70 @@ function formatPhone(fakePhoneNumberInput, phoneNumberInputId) {
                 }
             }
         }
-
-        // remplacer tous les caractères qui ne sont pas des nombres
-        phoneNumber = phoneNumber.replace(/\D/g,"");
-
-        // enlever les chiffres de trop
-        let phoneNumberWithMaxLength = "";
-        for(let i = 0; i < phoneNumber.length; i++) {
-            if(i < 10) {
-                phoneNumberWithMaxLength += phoneNumber[i];
-            }
+    
+        // récupération de la valeur brute du no de tel.
+        let phoneNumberRawData = getPhoneNumberRawData(phoneNumber);
+        if(rawDataInputId != undefined) {
+            document.querySelector("#" + rawDataInputId).value = phoneNumberRawData;
         }
-        phoneNumber = phoneNumberWithMaxLength;
-    
-        // ajouter la valeur à l'input
-        document.querySelector("#" + phoneNumberInputId).value = phoneNumber;
-    
-        // début formatage
-        let formattedPhoneNumber = "";
-    
-        if(phoneNumber.length > 0 && phoneNumber.length < 3) {
-            formattedPhoneNumber = `(${phoneNumber})`;
-        } else if(phoneNumber.length >= 3 && phoneNumber.length < 6) {
-            // format : (666) ***
-            formattedPhoneNumber = `(${phoneNumber[0]}${phoneNumber[1]}${phoneNumber[2]})`;
-            // ajout du reste
-            for(let i = 3; i < phoneNumber.length; i++) {
-                formattedPhoneNumber += phoneNumber[i];
-            }
-        } else if(phoneNumber.length >= 6) {
-            // format : (666) 666-****
-            formattedPhoneNumber = `(${phoneNumber[0]}${phoneNumber[1]}${phoneNumber[2]})`
-                                + `${phoneNumber[3]}${phoneNumber[4]}${phoneNumber[5]}-`;
-            // ajout du reste
-            for(let i = 6; i < phoneNumber.length; i++) {
-                formattedPhoneNumber += phoneNumber[i];
-            }
+        
+        // transformation de la valeur brute du no de tel. sous le format voulu
+        let formattedPhoneNumber = getFormattedPhoneNumber(phoneNumberRawData);
+        if(formattedDataInputId == undefined) {
+            document.querySelector("#" + formattedDataInputId).value = formattedPhoneNumber;
         }
-    
-        fakePhoneNumberInput.value =  formattedPhoneNumber;
     }
 }
 
-function formatPostalCode(fakePostalCodeInput, postalCodeInputId) {
-    if(fakePostalCodeInput != undefined && postalCodeInputId != undefined) {
-        let postalCode = fakePostalCodeInput.value;
+function getPhoneNumberRawData(phoneNumber) {
+    // remplacer tous les caractères qui ne sont pas des nombres
+    phoneNumber = phoneNumber.replace(/\D/g,"");
+
+    // enlever les chiffres de trop
+    let phoneNumberWithMaxLength = "";
+    for(let i = 0; i < phoneNumber.length; i++) {
+        if(i < 10) {
+            phoneNumberWithMaxLength += phoneNumber[i];
+        }
+    }
+    phoneNumber = phoneNumberWithMaxLength;
+
+    return phoneNumber;
+}
+
+function getFormattedPhoneNumber(phoneNumber){
+    // début formatage
+    let formattedPhoneNumber = "";
+        
+    if(phoneNumber.length > 0 && phoneNumber.length < 3) {
+        formattedPhoneNumber = `(${phoneNumber})`;
+    } else if(phoneNumber.length >= 3 && phoneNumber.length < 6) {
+        // format : (666) ***
+        formattedPhoneNumber = `(${phoneNumber[0]}${phoneNumber[1]}${phoneNumber[2]})`;
+        // ajout du reste
+        for(let i = 3; i < phoneNumber.length; i++) {
+            formattedPhoneNumber += phoneNumber[i];
+        }
+    } else if(phoneNumber.length >= 6) {
+        // format : (666) 666-****
+        formattedPhoneNumber = `(${phoneNumber[0]}${phoneNumber[1]}${phoneNumber[2]})`
+                            + `${phoneNumber[3]}${phoneNumber[4]}${phoneNumber[5]}-`;
+        // ajout du reste
+        for(let i = 6; i < phoneNumber.length; i++) {
+            formattedPhoneNumber += phoneNumber[i];
+        }
+    }
+
+    return formattedPhoneNumber;
+}
+
+function setPostalCode(valueContainerInput, rawDataInputId, formattedDataInputId) {
+    if(valueContainerInput != undefined) {
+        let postalCode = valueContainerInput.value;
 
         // supprimer un caractère si backspace est appuyé
         if(backspaceIsActive) {
-            let cursorPosition = fakePostalCodeInput.selectionStart;
+            let cursorPosition = valueContainerInput.selectionStart;
             if(postalCode[cursorPosition - 1] != undefined) {
                 if (cursorPosition == 3) {
                     postalCode = postalCode.substring(0, cursorPosition - 1) + postalCode.substring(cursorPosition, postalCode.length);
@@ -76,39 +91,54 @@ function formatPostalCode(fakePostalCodeInput, postalCodeInputId) {
             }
         }
 
-        // remplacer tous les caractères qui ne sont pas des chiffres ou des lettres
-        postalCode = postalCode.trim();
-        postalCode = postalCode.replace(/[^a-zA-Z0-9]/g,"");
-
-        // enlever les chiffres de trop
-        let postalCodeWithMaxLength = "";
-        for(let i = 0; i < postalCode.length; i++) {
-            if(i < 6) {
-                postalCodeWithMaxLength += postalCode[i];
-            }
+        // récupération de la valeur brute du code postal
+        let postalCodeRawData = getPostalCodeRawData(postalCode);
+        if(rawDataInputId != undefined) {
+            document.querySelector("#" + rawDataInputId).value = postalCodeRawData;
         }
-        postalCode = postalCodeWithMaxLength;
-
-        // forcer le code postal à être en majuscule
-        postalCode = postalCode.toUpperCase();
-
-        // ajouter la valeur à l'input
-        document.querySelector("#" + postalCodeInputId).value = postalCode;
-
-        // début formatage
-        let formattedPostalCode = "";
-
-        if(postalCode.length >= 3) {
-            // format : A0A ***
-            formattedPostalCode = `${postalCode[0]}${postalCode[1]}${postalCode[2]} `;
-            // ajout du reste
-            for(let i = 3; i < postalCode.length; i++) {
-                formattedPostalCode += postalCode[i];
-            }
-        } else {
-            formattedPostalCode = postalCode;
+        
+        // transformation de la valeur brute du code postal sous le format voulu
+        let formattedPostalCode = getFormattedPostalCode(postalCodeRawData);
+        if(formattedDataInputId == undefined) {
+            document.querySelector("#" + formattedDataInputId).value = formattedPostalCode;
         }
-
-        fakePostalCodeInput.value = formattedPostalCode;
     }
+}
+
+function getPostalCodeRawData(postalCode){
+    // remplacer tous les caractères qui ne sont pas des chiffres ou des lettres
+    postalCode = postalCode.trim();
+    postalCode = postalCode.replace(/[^a-zA-Z0-9]/g,"");
+
+    // enlever les chiffres de trop
+    let postalCodeWithMaxLength = "";
+    for(let i = 0; i < postalCode.length; i++) {
+        if(i < 6) {
+            postalCodeWithMaxLength += postalCode[i];
+        }
+    }
+    postalCode = postalCodeWithMaxLength;
+
+    // forcer le code postal à être en majuscule
+    postalCode = postalCode.toUpperCase();
+
+    return postalCode;
+}
+
+function getFormattedPostalCode(postalCode) {
+    // début formatage
+    let formattedPostalCode = "";
+
+    if(postalCode.length >= 3) {
+        // format : A0A ***
+        formattedPostalCode = `${postalCode[0]}${postalCode[1]}${postalCode[2]} `;
+        // ajout du reste
+        for(let i = 3; i < postalCode.length; i++) {
+            formattedPostalCode += postalCode[i];
+        }
+    } else {
+        formattedPostalCode = postalCode;
+    }
+
+    return formattedPostalCode;
 }
