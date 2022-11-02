5f31419616690df376e2ea748f02e7e1688ffccd
Samuel Mongeon <2036516@cegepgranby.qc.ca>
2022-11-02 09:16:21 -0400

    feat: ajout vm gestion étudiants
    Ajout du view model pour recevoir un liste des étudiants.

---
+++ projet/vlissides-bibliotheque/vlissides-bibliotheque/ViewModels/GestionEtudiantsVM.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.Build.Framework;
+using vlissides_bibliotheque.Models;
+
+namespace vlissides_bibliotheque.ViewModels
+{
+    public class GestionEtudiantsVM
+    {
+        public List<Etudiant> Etudiants { get; set; }
+    }
+}
