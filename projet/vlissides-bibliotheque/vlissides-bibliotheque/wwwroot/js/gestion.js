cd9ca85b447780b28d8c78111542e5bc8b74b06f
Samuel Mongeon <2036516@cegepgranby.qc.ca>
2022-11-03 14:18:28 -0400

    feat: ajout contenu de base js et liaison projet
    Ajout du lien script vers le fichier js ajouté pour la gestion et ajout
    du contenu de base pour les requêtes ajax.

--- projet/vlissides-bibliotheque/vlissides-bibliotheque/wwwroot/js/gestion.js
+++ projet/vlissides-bibliotheque/vlissides-bibliotheque/wwwroot/js/gestion.js
@@ -1,1 +1,36 @@
 ﻿
+function getFormulaireModifierEtudiant(id) {
+
+    fetch("/Accueil/ChangerPrix" + String(id), {
+        method: 'GET',
+    }).then(function (res) {
+        if (!res.ok) {
+            alert("L'étudiant est introuvable.")
+        }
+
+        res.text().then(function (res) {
+            document.querySelector("#etudiant-" + String(id))
+                .querySelector(".modal-body").innerHTML = res;
+        });
+    });
+}
+
+function modifierEtudiant() {
+    //fetch("/Accueil/ChangerPrix" + String(id), {
+    //    method: 'GET',
+    //    body: data,
+    //    contentType: "application/json; charset=utf-8",
+    //    headers: {
+    //        "Content-Type": "application/json",
+    //    }
+    //}).then(function (res) {
+    //    if (!res.ok) {
+    //        alert("L'étudiant est introuvable.")
+    //    }
+
+    //    res.text().then(function (res) {
+    //        document.querySelector("#etudiant-" + String(id))
+    //            .querySelector(".modal-body").innerHTML = res;
+    //    });
+    //});
+}
