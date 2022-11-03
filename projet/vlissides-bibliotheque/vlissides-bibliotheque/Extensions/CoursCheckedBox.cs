f1f1db90e6336c12f97039789239c16ccf8e910b
VINCENT CHOUINARD <1624901@CegepGranby.qc.ca>
2022-11-02 20:15:30 -0400

    feat: extension pour trouver les cours extension pour trouver les cours associer à l'étudiant connecter

---
+++ projet/vlissides-bibliotheque/vlissides-bibliotheque/Extensions/CoursCheckedBox.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using System.Net.NetworkInformation;
+using vlissides_bibliotheque.Data;
+using vlissides_bibliotheque.Models;
+using vlissides_bibliotheque.ViewModels;
+
+namespace vlissides_bibliotheque
+{
+    public static class CoursCheckedBox
+    {
+        public static List<checkBoxCours> GetCoursCheckedBox(ApplicationDbContext _context, string etudiantId)
+        {
+            List<checkBoxCours> listCheckedBox = new();
+            List<Cours> coursBD = _context.Cours
+                .Include(x=>x.ProgrammeEtude)
+                .ToList();
+            List<CoursEtudiant> listCoursEtudiant = _context.CoursEtudiants
+                .Include(x => x.Cours)
+                .Include(x => x.Etudiant)
+                .ToList();
+            List<CoursEtudiant> coursAssocierEtudiant = listCoursEtudiant.FindAll(x => x.EtudiantId == etudiantId);
+
+            foreach(Cours cours in coursBD)
+            {
+                checkBoxCours checkBox = new();
+                checkBox.Cours = cours;
+                checkBox.Cocher = false;
+                listCheckedBox.Add(checkBox);
+
+            }
+
+            foreach(CoursEtudiant coursEtudiant in coursAssocierEtudiant)
+            {
+                listCheckedBox.Find(x => x.Cours.CoursId == coursEtudiant.CoursId).Cocher = true;
+
+            }
+            return listCheckedBox;
+        }
+    }
+}
