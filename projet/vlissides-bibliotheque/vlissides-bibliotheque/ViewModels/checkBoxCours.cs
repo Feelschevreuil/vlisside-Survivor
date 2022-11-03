7910e2015cea7b87bd805628f4945a99e613aaf4
VINCENT CHOUINARD <1624901@CegepGranby.qc.ca>
2022-11-02 20:13:51 -0400

    feat: Js et DTO pour la requet AJAX

---
+++ projet/vlissides-bibliotheque/vlissides-bibliotheque/ViewModels/checkBoxCours.cs
@@ -0,0 +1,11 @@
+﻿using vlissides_bibliotheque.Models;
+
+namespace vlissides_bibliotheque.ViewModels
+{
+    public class checkBoxCours
+    {
+        public Cours Cours { get; set; }
+
+        public bool Cocher { get; set; }
+    }
+}
---
+++ projet/vlissides-bibliotheque/vlissides-bibliotheque/wwwroot/js/gestionCours.js
@@ -0,0 +1,29 @@
+﻿function assignerCours(CoursId) {
+
+    var Cocher = document.querySelector('#' + "Cours" + "-" + CoursId).checked
+
+    var DonnerRecus =
+    {
+        CoursId: CoursId,
+        cocher: Cocher,
+
+    };
+
+
+    var data = JSON.stringify(DonnerRecus);
+
+
+    fetch("/GestionProfil/AssignerCours", {
+        method: 'Post',
+        body: data,
+        contentType: "application/json; charset=utf-8",
+        headers: {
+            "Content-Type": "application/json",
+        },
+
+    }).then(function (response) {
+
+        return null;
+
+    });
+}
