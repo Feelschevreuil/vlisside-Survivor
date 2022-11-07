30b7bd68bf6dcef0ea8e70be4e7e306c1d97e9e0
SAMORY ROY <1855390@CegepGranby.qc.ca>
2022-11-07 13:31:13 -0500

    feat: controller pour le panier

---
+++ projet/vlissides-bibliotheque/vlissides-bibliotheque/Controllers/PanierController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using vlissides_bibliotheque.Data;
+
+namespace vlissides_bibliotheque.Controllers
+{
+    [Authorize]
+    public class PanierController : Controller
+    {
+        private readonly ILogger<AccueilController> _logger;
+        private readonly ApplicationDbContext _context;
+
+        public PanierController(ILogger<AccueilController> logger, ApplicationDbContext context)
+        {
+            _logger = logger;
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
