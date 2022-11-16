5838ea1a4e640c4693dad9c1e084fa2798f6d724
SAMORY ROY <1855390@CegepGranby.qc.ca>
2022-11-16 10:00:06 -0500

    feat: finission du script
    ajoute de 2 fonctions, une pour l'ouverture de la page, une pour la suppression du localstorage, plus quelques modifications de fonction déjà existante

--- projet/vlissides-bibliotheque/vlissides-bibliotheque/wwwroot/js/LivreDetail.js
+++ projet/vlissides-bibliotheque/vlissides-bibliotheque/wwwroot/js/LivreDetail.js
@@ -1,13 +1,18 @@
-﻿function BoxChecked(LivreId, etat) {
-    var baliseEtat = document.querySelector('#' + etat + "-" + LivreId);
-    var parent = baliseEtat.parentElement;
-    for (let input of parent.querySelectorAll("label")) {
-        input.classList.remove("bg-back");
-        input.classList.add("bg-top");
+﻿initDetail()
 
-    };
-    baliseEtat.classList.add("bg-back");
-    baliseEtat.classList.remove("bg-top");
+function BoxChecked(LivreId, etat) {
+    var boutonPanierSupprimer = document.getElementById("supprimer")
+    if (boutonPanierSupprimer.hidden) {
+        var baliseEtat = document.querySelector('#' + etat + "-" + LivreId);
+        var parent = baliseEtat.parentElement;
+        for (let input of parent.querySelectorAll("label")) {
+            input.classList.remove("bg-back");
+            input.classList.add("bg-top");
+
+        };
+        baliseEtat.classList.add("bg-back");
+        baliseEtat.classList.remove("bg-top");
+    }
 }
 
 function CopierCourriel() {
@@ -26,6 +31,8 @@ function ajouterLivreLocalStorage(id) {
     var updateLocalStorage = true;
     var etatLivre = document.getElementsByClassName("bg-back")[0].innerText
     var p = document.getElementById("pErreur");
+    var supprimer = document.getElementById("supprimer")
+    var ajouter = document.getElementById("ajouter")
     p.innerText = "";
 
     if (livres != null || livres != undefined) {
@@ -37,7 +44,7 @@ function ajouterLivreLocalStorage(id) {
                         idTrouveNonModifie = true;
                     }
                     else {
-                        livres.Neuf.splice(j,1);
+                        livres.Neuf.splice(j, 1);
                     }
                 }
             }
@@ -107,18 +114,24 @@ function ajouterLivreLocalStorage(id) {
                 localStorage.clear();
                 localStorage.setItem('itemsPanier', JSON.stringify(livres));
                 updateLocalStorage = true;
+                ajouter.hidden = true;
+                supprimer.hidden = false;
                 break;
             case "Usagé":
                 livres.Usage.push(id.toString());
                 localStorage.clear();
                 localStorage.setItem('itemsPanier', JSON.stringify(livres));
                 updateLocalStorage = true;
+                ajouter.hidden = true;
+                supprimer.hidden = false;
                 break;
             case "Numérique":
                 livres.Numerique.push(id.toString());
                 localStorage.clear();
                 localStorage.setItem('itemsPanier', JSON.stringify(livres));
                 updateLocalStorage = true;
+                ajouter.hidden = true;
+                supprimer.hidden = false;
                 break;
             default:
                 updateLocalStorage = false;
@@ -127,10 +140,104 @@ function ajouterLivreLocalStorage(id) {
     else {
         var p = document.getElementById("pErreur");
         p.innerText = "* Ce livre se trouve déjà dans votre panier";
+        ajouter.hidden = true;
+        supprimer.hidden = false;
     }
 
     if (!updateLocalStorage) {
         var p = document.getElementById("pErreur");
         p.innerText = "* L'état du livre que vous essayez d'ajouter à votre panier est invalide, sélectionnez un autre état."
+    }
+}
+
+function supprimerLivreLocalStorage(id) {
+    var supprimer = document.getElementById("supprimer")
+    var ajouter = document.getElementById("ajouter")
+    var livres = JSON.parse(localStorage.getItem('itemsPanier'))
+    var idTrouveModifie = false;
+    var p = document.getElementById("pErreur");
+    p.innerText = "";
+
+
+    if (livres != null || livres != undefined) {
+
+        if (livres.Neuf != null) {
+            for (var j = 0; j < livres.Neuf.length; j++) {
+                if (livres.Neuf[j] == id) {
+                    livres.Neuf.splice(j, 1);
+                    idTrouveModifie = true;
+                }
+            }
+        }
+        if (livres.Usage != null) {
+            for (var j = 0; j < livres.Usage.length; j++) {
+                if (livres.Usage[j] == id) {
+                    livres.Usage.splice(j, 1);
+                    idTrouveModifie = true;
+                }
+            }
+        }
+        if (livres.Numerique != null) {
+            for (var j = 0; j < livres.Numerique.length; j++) {
+                if (livres.Numerique[j] == id) {
+                    livres.Numerique.splice(j, 1);
+                    idTrouveModifie = true;
+                }
+            }
+        }
+
+    }
+
+    if (idTrouveModifie) {
+        localStorage.setItem('itemsPanier', JSON.stringify(livres))
+        ajouter.hidden = false;
+        supprimer.hidden = true;
+    }
+    else {
+        p.innerText = "Erreur lors de la suppression du livre"
+    }
+
+}
+
+function initDetail() {
+    var livres = JSON.parse(localStorage.getItem('itemsPanier'));
+    var supprimer = document.getElementById("supprimer");
+    var ajouter = document.getElementById("ajouter");
+    var idLivre = document.getElementById("idLivre");
+    var id = idLivre.innerText;
+
+
+    if (livres != null || livres != undefined) {
+
+        if (livres.Neuf != null) {
+            for (var j = 0; j < livres.Neuf.length; j++) {
+                if (livres.Neuf[j] == id) {
+                    idTrouveModifie = true;
+                    BoxChecked(id, "Neuf");
+                }
+            }
+        }
+        if (livres.Usage != null) {
+            for (var j = 0; j < livres.Usage.length; j++) {
+                if (livres.Usage[j] == id) {
+                    idTrouveModifie = true;
+                    BoxChecked(id, "Usager");
+                }
+            }
+        }
+        if (livres.Numerique != null) {
+            for (var j = 0; j < livres.Numerique.length; j++) {
+                if (livres.Numerique[j] == id) {
+                    idTrouveModifie = true;
+                    BoxChecked(id, "Numerique");
+                }
+            }
+        }
+
+    }
+
+    if (idTrouveModifie) {
+        supprimer.hidden = false;
+        ajouter.hidden = true;
     }
 }
