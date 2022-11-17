1f53b1fb34447ff3baba27a29dc10c76c92452eb
VINCENT CHOUINARD <1624901@CegepGranby.qc.ca>
2022-11-17 15:30:31 -0500

    fix: decimal dans le panier

--- projet/vlissides-bibliotheque/vlissides-bibliotheque/Extensions/AffichagePrix.cs
+++ projet/vlissides-bibliotheque/vlissides-bibliotheque/Extensions/AffichagePrix.cs
@@ -67,6 +67,34 @@ namespace vlissides_bibliotheque
             return "";
         }
 
+        public static string GetPremierPrix (TuileLivreBibliotequeVM Model)
+        {
+            double prixInitial = 0;
+            string Prixdecimal = "";
+            List<PrixEtatLivre> prixEtatLivres = new();
+
+            if (Model.prixEtatLivre != null && Model.prixEtatLivre.Count() > 0)
+            {
+                foreach(PrixEtatLivre etatLivre in Model.prixEtatLivre)
+                {
+                    if(etatLivre != null)
+                    {
+
+                        prixEtatLivres.Add(etatLivre);
+                    }
+                }
+
+                prixInitial = prixEtatLivres.FirstOrDefault().Prix;
+                prixInitial = Math.Floor(100 * prixInitial) / 100;
+                if (prixInitial == (int)prixInitial)
+                {
+                    Prixdecimal = ",00";
+                    return prixInitial + Prixdecimal;
+                }
+                return prixInitial.ToString();
+            }
+            return "";
+        }
     }
 }
 
