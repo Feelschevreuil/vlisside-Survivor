d0dc7c3bf8a593e8bbee50b8d8b3150bf3bf05dc
rob <2060115@cegepgranby.qc.ca>
2022-11-17 15:43:51 -0500

    refactor: tabulation du dao utils

--- projet/vlissides-bibliotheque/vlissides-bibliotheque/DAO/DAOUtils.cs
+++ projet/vlissides-bibliotheque/vlissides-bibliotheque/DAO/DAOUtils.cs
@@ -7,38 +7,38 @@ namespace vlissides_bibliotheque.DAO
     public class DAOUtils
     {
 
-	/// <summary>
-	/// Calcule la quantité de pages selon le nombre total d'éléments et le nombre d'éléments par page.
-	/// </summary>
-	public int GetNombrePages(int quantiteEnregistrements,int quantiteParPage)
-	{
+        /// <summary>
+        /// Calcule la quantité de pages selon le nombre total d'éléments et le nombre d'éléments par page.
+        /// </summary>
+        public int GetNombrePages(int quantiteEnregistrements,int quantiteParPage)
+        {
 
-	    double nombreDePages;
+            double nombreDePages;
 
-	    nombreDePages = quantiteEnregistrements / quantiteParPage;
-	    
-	    return (int)Math.Ceiling(nombreDePages);
-	}
+            nombreDePages = quantiteEnregistrements / quantiteParPage;
+            
+            return (int)Math.Ceiling(nombreDePages);
+        }
 
-	/// <summary>
-	/// Calcule la quantité d'enregistrements à sauter selon le nombre denregistrements affichées par page.
-	/// <param name="elementsPerPage">Enregistrements à afficher par page.</param>
-	/// <param name="pageToGet">Enregistrements à aller chercher.</param>
-	/// </summary>
-	public static int GetQuantityOfElementsToSkip(int elementsPerPage, int pageToGet)
-	{
+        /// <summary>
+        /// Calcule la quantité d'enregistrements à sauter selon le nombre denregistrements affichées par page.
+        /// <param name="elementsPerPage">Enregistrements à afficher par page.</param>
+        /// <param name="pageToGet">Enregistrements à aller chercher.</param>
+        /// </summary>
+        public static int GetQuantityOfElementsToSkip(int elementsPerPage, int pageToGet)
+        {
 
-	    if(pageToGet > 0)
-	    {
+            if(pageToGet > 0)
+            {
 
-		int elementsToSkip;
+                int elementsToSkip;
 
-		elementsToSkip = elementsPerPage * pageToGet;
+                elementsToSkip = elementsPerPage * pageToGet;
 
-		return elementsToSkip;
-	    }
+                return elementsToSkip;
+            }
 
-	    return 0;
-	}
+            return 0;
+        }
     }
 }
