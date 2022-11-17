2f9f2f0b6fc417b8f090a142180cc89a6baf6fe3
rob <2060115@cegepgranby.qc.ca>
2022-11-17 15:46:50 -0500

    refactor: tabulations idao recherche avancée

--- projet/vlissides-bibliotheque/vlissides-bibliotheque/DAO/IDAORecherchableAvancee.cs
+++ projet/vlissides-bibliotheque/vlissides-bibliotheque/DAO/IDAORecherchableAvancee.cs
@@ -10,21 +10,21 @@ namespace vlissides_bibliotheque.DAO
     /// </summary>
     interface IDAORecherchableAvancee<T,J>
     {
-	
-	/// <summary>Cherche les objets par leurs propriétés.</summary>
-	/// <param name="proprietes">
-	/// Objet contenant les propriétés à chercher.
-	/// </param>
-	/// <param name="quantiteParPage">
-	/// La quantité d'objets que l'on veut afficher par page.
-	/// </param>
-	/// <param name="page">
-	/// Le numéro de page des résultats.
-	/// </param>
-	/// <returns>
-	/// Une liste d'objets ayant les propriétés désirées ou une liste vide 
-	/// s'il n'y en a pas.
-	/// </returns>
-	ICollection<T> GetSelonProprietes(J proprietes, int quantiteParPage = ConstantesDAO.QUANTITE_PAR_PAGE, int page = 0);
+    
+        /// <summary>Cherche les objets par leurs propriétés.</summary>
+        /// <param name="proprietes">
+        /// Objet contenant les propriétés à chercher.
+        /// </param>
+        /// <param name="quantiteParPage">
+        /// La quantité d'objets que l'on veut afficher par page.
+        /// </param>
+        /// <param name="page">
+        /// Le numéro de page des résultats.
+        /// </param>
+        /// <returns>
+        /// Une liste d'objets ayant les propriétés désirées ou une liste vide 
+        /// s'il n'y en a pas.
+        /// </returns>
+        ICollection<T> GetSelonProprietes(J proprietes, int quantiteParPage = ConstantesDAO.QUANTITE_PAR_PAGE, int page = 0);
     }
 }
