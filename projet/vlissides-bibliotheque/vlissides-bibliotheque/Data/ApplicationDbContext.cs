57d907d65934a6a86d804de4db463061c9578d31
rob <2060115@cegepgranby.qc.ca>
2022-11-16 21:58:27 -0500

    Revert "feat: préparation aux tests unitaires"
    This reverts commit a34a82dc6b7beefd6020cc6bbff5343456f61037.
    
    Cause un merge conflict et ne servent pas. Une BD en ram est prise au lieu d'utiliser les modifications originales du commit en question.

--- projet/vlissides-bibliotheque/vlissides-bibliotheque/Data/ApplicationDbContext.cs
+++ projet/vlissides-bibliotheque/vlissides-bibliotheque/Data/ApplicationDbContext.cs
@@ -9,7 +9,7 @@ namespace vlissides_bibliotheque.Data
     /// <summary>
     /// Classe <c>ApplicationDbContext</c> définit la compostion de la base de données.
     /// </summary>
-    public class ApplicationDbContext : IdentityDbContext, IDbContext
+    public class ApplicationDbContext : IdentityDbContext
 	{
 		public DbSet<Adresse> Adresses { get; set; }
 		public DbSet<Auteur> Auteurs { get; set; }
@@ -50,21 +50,160 @@ namespace vlissides_bibliotheque.Data
 		{
 			base.OnModelCreating(builder);
 
-			DbContextConfiguration dbContextConfiguration;
+			CreerTableUtilisateurs(builder);
 
-			dbContextConfiguration = new(this);
+			CreerEtatLivre(builder);
 
-			dbContextConfiguration.CreerTableUtilisateurs(builder);
+			CreerRoles(builder);
 
-			dbContextConfiguration.CreerEtatLivre(builder);
+			CreerAdmin(builder);
 
-			dbContextConfiguration.CreerRoles(builder);
+			CreerTablesLiaison(builder);
 
-			dbContextConfiguration.CreerAdmin(builder);
+			CreerDoubleFK(builder);
+		}
 
-			dbContextConfiguration.CreerTablesLiaison(builder);
+		/// <summary>
+		/// Génère les tables d'utilisateurs.
+		/// </summary>
+		/// <param name="builder"></param>
+		private void CreerTableUtilisateurs(ModelBuilder builder)
+		{
+			builder.Entity<Utilisateur>().ToTable(nameof(Utilisateurs));
+			builder.Entity<Etudiant>().ToTable(nameof(Etudiants));
+		}
 
-			dbContextConfiguration.CreerDoubleFK(builder);
+		/// <summary>
+		/// Crée les états de livre.
+		/// </summary>
+		/// <param name="builder"></param>
+		private void CreerEtatLivre(ModelBuilder builder)
+		{
+			List<EtatLivre> EtatLivres = new() {
+				new EtatLivre() {
+					EtatLivreId = 1,
+					Nom = NomEtatLivre.Neuf
+				},
+				new EtatLivre() {
+					EtatLivreId = 2,
+					Nom = NomEtatLivre.Usagee
+				},
+				new EtatLivre() {
+					EtatLivreId = 3,
+					Nom = NomEtatLivre.Numerique
+				}
+			};
+
+			builder.Entity<EtatLivre>().HasData(EtatLivres);
+		}
+
+		/// <summary>
+		/// Crée les rôles d'utilisateur.
+		/// </summary>
+		/// <param name="builder"></param>
+		private void CreerRoles(ModelBuilder builder)
+		{
+			
+			// UTILISATEUR
+			const string ROLE_UTILISATEUR_ID = "c2c54011-c8a3-44b7-a560-b76da1383d79";
+			const string ROLE_UTILISATEUR_CONCURRENCYSTAMP = "69162fbd-767b-4ecd-8cc9-fd1fe2e0322f";
+			// ETUDIANT
+			const string ROLE_ETUDIANT_ID = "c7a578b8-1d4b-43c3-a85e-179d132e2aed";
+			const string ROLE_ETUDIANT_CONCURRENCYSTAMP = "9985b076-ab9a-4538-b692-34b21ed3d2e6";
+			// AJOUT DES RÔLES
+			builder.Entity<IdentityRole>().HasData(new List<IdentityRole>(){
+				new IdentityRole()
+				{
+					Id = ROLE_ADMIN_ID,
+					Name = RolesName.Admin,
+					NormalizedName = RolesName.Admin.ToUpper(),
+					ConcurrencyStamp = ROLE_ADMIN_CONCURRENCYSTAMP
+				},
+				new IdentityRole(){
+					Id = ROLE_UTILISATEUR_ID,
+					Name = RolesName.Utilisateur,
+					NormalizedName = RolesName.Utilisateur.ToUpper(),
+					ConcurrencyStamp = ROLE_UTILISATEUR_CONCURRENCYSTAMP
+				},
+				new IdentityRole()
+				{
+					Id = ROLE_ETUDIANT_ID,
+					Name = RolesName.Etudiant,
+					NormalizedName = RolesName.Etudiant.ToUpper(),
+					ConcurrencyStamp = ROLE_ETUDIANT_CONCURRENCYSTAMP
+				}
+			});
+		}
+
+		/// <summary>
+		/// Crée un administrateur initial.
+		/// </summary>
+		/// <param name="builder"></param>
+		private void CreerAdmin(ModelBuilder builder)
+		{
+			// Obj pour HASHER un mot de passe
+			PasswordHasher<Utilisateur> passwordHasher = new();
+			// AJOUT D'UN ADMIN
+			builder.Entity<Utilisateur>().HasData(new Utilisateur() {
+				Id = USER_ADMIN_ID,
+				Email = Emails.EmailAdmin,
+		EmailConfirmed = true,
+				NormalizedEmail = Emails.EmailAdmin.ToUpper(),
+				UserName = Emails.EmailAdmin,
+				NormalizedUserName = Emails.EmailAdmin.ToUpper(),
+				Nom = "John",
+				Prenom = "Gordon",
+				ConcurrencyStamp = USER_ADMIN_CONCURRENCYSTAMP,
+				PasswordHash = passwordHasher.HashPassword(null, "Jaimelaprog1!")
+			});
+
+			// Lier utilisateur et rôle
+			builder.Entity<IdentityUserRole<string>>().HasData(new List<IdentityUserRole<string>>(){
+				new IdentityUserRole<string>()
+				{
+					RoleId = ROLE_ADMIN_ID,
+					UserId = USER_ADMIN_ID
+				}
+			});
+		}
+
+		/// <summary>
+		/// Crée les tables de liaison.
+		/// </summary>
+		/// <param name="builder"></param>
+		private void CreerTablesLiaison(ModelBuilder builder)
+		{
+			builder.Entity<AuteurLivre>().HasKey(auteurLivre => new { auteurLivre.AuteurId, auteurLivre.LivreBibliothequeId });
+
+			builder.Entity<CommandeEtudiant>().HasKey(CommandeEtudiant => new { CommandeEtudiant.FactureEtudiantId, CommandeEtudiant.PrixEtatLivreId });
+
+			builder.Entity<EvaluationLivre>().HasKey(evaluationLivre => new { evaluationLivre.EvaluationId, evaluationLivre.LivreBibliothequeId });
+
+			builder.Entity<CoursProfesseur>().HasKey(coursProfesseur => new { coursProfesseur.CoursId, coursProfesseur.ProfesseurId });
+
+			builder.Entity<CoursEtudiant>().HasKey(coursEtudiant => new { coursEtudiant.CoursId, coursEtudiant.EtudiantId });
+		}
+
+		/// <summary>
+		/// Gère les doubles liaisons.
+		/// </summary>
+		/// <param name="builder"></param>
+		private void CreerDoubleFK(ModelBuilder builder)
+		{
+			builder.Entity<Etudiant>()
+			   .HasOne(m => m.ProgrammeEtude)
+			   .WithMany()
+			   .OnDelete(DeleteBehavior.NoAction);
+
+			builder.Entity<Etudiant>()
+				.HasOne(m => m.Adresse)
+				.WithMany()
+				.OnDelete(DeleteBehavior.NoAction);
+
+			builder.Entity<CoursLivre>()
+				.HasOne(m => m.LivreBibliotheque)
+				.WithMany()
+				.OnDelete(DeleteBehavior.NoAction);
 		}
 	}
 }
