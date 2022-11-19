6b4922997b6f3a66cff82c8a18621f99ae1000a3
rob <2060115@cegepgranby.qc.ca>
2022-11-19 17:51:57 -0500

    feat: séparation des interfaces dao pour supporter les tables de liaison

--- projet/vlissides-bibliotheque/vlissides-bibliotheque/DAO/IDAO.cs
+++ projet/vlissides-bibliotheque/vlissides-bibliotheque/DAO/IDAO.cs
@@ -9,13 +9,6 @@ namespace vlissides_bibliotheque.DAO
     {
 
         /// <summary>
-        /// Cherche l'objet correspondant avec l'id.
-        /// </summary>
-        /// <param name="id">L'id de l'objet à chercher.</param>
-        /// <returns>L'object correspondant à l'objet.</returns>
-        T Get(long id);
-
-        /// <summary>
         /// Cherche tous les objets.
         /// </summary>
         /// <returns>Les objets en liste.</returns>
@@ -25,22 +18,7 @@ namespace vlissides_bibliotheque.DAO
         /// Sauvegarde l'objet désiré.
         /// </summary>
         /// <param name="t">L'objet à sauvegarder.</param>
-        /// <returns>true si l'objet a été sauvegardé avec succès.</returns>
+        /// <returns>L'objet modifié.</returns>
         bool Save(T t);
-
-        /// <summary>
-        /// Met à jour l'objet désiré.
-        /// </summary>
-        /// <param name="idObjetOriginal">L'objet contenant les propriétés originales</param>
-        /// <param name="objetAJour">L'objet contenant les modifications.</param>
-        /// <returns>true si l'objet a été sauvegardé avec succès.</returns>
-        T Update(int idObjetOriginal, T objetAJour);
-
-        /// <summary>
-        /// Efface l'objet désiré.
-        /// </summary>
-        /// <param name="id">L'id de l'objet à effacer.</param>
-        /// <returns>true si l'objet a été effacé avec succès.</returns>
-        bool Delete(long id);
     }
 }
