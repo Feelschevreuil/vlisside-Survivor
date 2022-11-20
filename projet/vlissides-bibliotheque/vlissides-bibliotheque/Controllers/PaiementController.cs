1191f37d4e03f1d30d142d9728be5fbcdef785d8
rob <2060115@cegepgranby.qc.ca>
2022-11-20 17:56:17 -0500

    tâche: route temporaire pour tester un paiement

--- projet/vlissides-bibliotheque/vlissides-bibliotheque/Controllers/PaiementController.cs
+++ projet/vlissides-bibliotheque/vlissides-bibliotheque/Controllers/PaiementController.cs
@@ -7,10 +7,14 @@ using vlissides_bibliotheque.DTO;
 using vlissides_bibliotheque.Services;
 using vlissides_bibliotheque.Constantes;
 using vlissides_bibliotheque.ViewModels;
+using vlissides_bibliotheque.Enums;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Newtonsoft.Json;
+
+//TODO: delete DAO ref
+using vlissides_bibliotheque.DAO;
 
 namespace vlissides_bibliotheque.Controllers
 {
@@ -42,7 +46,7 @@ namespace vlissides_bibliotheque.Controllers
         // POST: /paiements
         // GET pour le moment, pour tester
         // [HttpPost]
-        public async Task<IActionResult> Index()//[FromBody] FactureEtudiantDTO factureEtudiantDTO)
+        public async Task<IActionResult> Index2()//[FromBody] FactureEtudiantDTO factureEtudiantDTO)
         {
 
             /*
@@ -97,6 +101,62 @@ namespace vlissides_bibliotheque.Controllers
             */
 
             //return Content("What did you send? : " + userId2);
+        }
+
+        // GET: /paiement/test/
+        // TODO: enlever! route uniquement pour le design!
+        public async Task<IActionResult> Index()
+        {
+
+            List<CommandePartielleVM> commandesPartielles;
+            PaiementVM paiementVM;
+            Etudiant etudiant;
+
+            etudiant = await _userManagerEtudiant
+                    .GetUserAsync(HttpContext.User);
+
+            commandesPartielles = new()
+            {
+                new CommandePartielleVM() 
+                {
+                    EtatLivre = EtatLivreEnum.NEUF,
+                    Isbn = "6666666669",
+                    PrixUnitaireGele = 69.99,
+                    Quantite = 1,
+                    Titre = "foobar 0"
+                },
+                new CommandePartielleVM() 
+                {
+                    EtatLivre = EtatLivreEnum.NUMERIQUE,
+                    Isbn = "6666666669",
+                    PrixUnitaireGele = 69.99,
+                    Quantite = 1,
+                    Titre = "foobar 1"
+                },
+                new CommandePartielleVM() 
+                {
+                    EtatLivre = EtatLivreEnum.USAGE,
+                    Isbn = "6666666669",
+                    PrixUnitaireGele = 69.99,
+                    Quantite = 1,
+                    Titre = "foobar 2"
+                }
+            };
+
+            paiementVM = new()
+            {
+                PublicApiKey = "pk_test_51M59yHIKN7Q7tR49S3LZio0cmMAIwrANCcJ0SbvSQlfyslPoKR3GTHJmApQzoxlYKwqjNrDN3sfOFx7FS71o5i1T008GU1VoRg",
+                PaymentIntentId = "pi_3M6LUeIKN7Q7tR491FXtOGwz",
+                ClientSecret = "pi_3M6LUeIKN7Q7tR491FXtOGwz_secret_IhEOrxoyxMFQOv63zlWpNEEKS",
+                AdresseLivraison = etudiant.Adresse,
+                CommandesPartielles = commandesPartielles,
+                Tvq = 0.0M,
+                Tps = 0.05M,
+                Total = 999.99,
+                StatutFacture = StatusFacture.ATTENTE_PAIEMENT
+            };
+
+            return View(paiementVM);
         }
     }
 }
