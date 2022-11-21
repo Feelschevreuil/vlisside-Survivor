bd4196ede01946033df43fe25b6baf6aeb770038
SAMORY ROY <1855390@CegepGranby.qc.ca>
2022-11-21 14:18:03 -0500

    feat: affiche le bon étatlivre sur la cards  si dans le panier

--- projet/vlissides-bibliotheque/vlissides-bibliotheque/wwwroot/js/updateBoutonAjoutPanier.js
+++ projet/vlissides-bibliotheque/vlissides-bibliotheque/wwwroot/js/updateBoutonAjoutPanier.js
@@ -11,6 +11,7 @@ function initiationCards() {
                 if (!(ajouter == null || supprimer == null)) {
                     ajouter.hidden = true;
                     supprimer.hidden = false;
+
                 }
             }
         }
@@ -21,6 +22,24 @@ function initiationCards() {
                 if (!(ajouter == null || supprimer == null)) {
                     ajouter.hidden = true;
                     supprimer.hidden = false;
+
+                    var neuf = document.getElementById("Neuf-" + livres.Usage[j])
+                    var usage = document.getElementById("Usager-" + livres.Usage[j])
+                    var numerique = document.getElementById("Numerique-" + livres.Usage[j])
+
+                    if (!usage.classList.contains("bg-back")) {
+                        if (neuf != null) {
+                            neuf.classList.remove("bg-back")
+                            neuf.classList.add("bg-top")
+                            usage.classList.add("bg-back")
+                            usage.classList.remove("bg-top")
+                        } else if (neuf == null && numerique != null) {
+                            numerique.classList.remove("bg-back")
+                            numerique.classList.add("bg-top")
+                            usage.classList.add("bg-back")
+                            usage.classList.remove("bg-top")
+                        }
+                    }
                 }
             }
         }
@@ -31,6 +50,20 @@ function initiationCards() {
                 if (!(ajouter == null || supprimer == null)) {
                     ajouter.hidden = true;
                     supprimer.hidden = false;
+
+                    var neuf = document.getElementById("Neuf-" + livres.Numerique[j])
+                    var usage = document.getElementById("Usager-" + livres.Numerique[j])
+                    var numerique = document.getElementById("Numerique-" + livres.Numerique[j])
+
+                    if (!numerique.classList.contains("bg-back")) {
+                        if (neuf != null) {
+                            neuf.classList.remove("bg-back")
+                            neuf.classList.add("bg-top")
+                            numerique.classList.add("bg-back")
+                            numerique.classList.remove("bg-top")
+                        }
+                    }
+
                 }
             }
         }
