86299f37d9be925b0ac2c3bd386188f20c3cf408
Samuel Mongeon <2036516@cegepgranby.qc.ca>
2022-11-24 11:43:35 -0500

    feat: création data annotation de téléphone
    Créer le data annotation pour le numéro de téléphone en se basant sur le
    fichier du data annotation du isbn. Il n'y avait qu'à modifier les
    commentaires et les conditions.

---
+++ projet/vlissides-bibliotheque/vlissides-bibliotheque/Validation/TelephoneAttribute.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace vlissides_bibliotheque.Validation
+{
+    /// <summary>
+    /// Classe <c>TelephoneAttribute</c> hérite de <c>ValidationAttribute</c> et génère un data annotation.
+    /// </summary>
+    public class TelephoneAttribute : ValidationAttribute
+	{
+        /// <summary>
+        /// Retourne un résultat de validation (une erreur ou un succès) par rapport à la taille 
+        /// du numéro de téléphone et au type voulu (int).
+        /// </summary>
+        /// <param name="value"></param>
+        /// <param name="validationContext"></param>
+        /// <returns>Un <c>ValidationResult</c></returns>
+        protected override ValidationResult? IsValid(object? value, ValidationContext validationContext)
+		{
+			string Telephone = (string)value;
+
+            bool isInt = Int32.TryParse(Telephone, out int valueInt);
+
+            if (Telephone != null && Telephone.Length == 10 && isInt) 
+			{
+                return ValidationResult.Success;
+            }
+
+            return new ValidationResult(ErrorMessage = $"Le téléphone doit contenir 10 chiffres.");
+		}
+	}
+}
