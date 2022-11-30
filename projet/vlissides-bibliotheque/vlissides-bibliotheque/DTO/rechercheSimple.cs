eee82f71458db9ef35d7dcda9a283e7d57663901
SAMORY ROY <1855390@CegepGranby.qc.ca>
2022-11-30 14:10:03 -0500

    feat: DTO pour recherche rapiDe

---
+++ projet/vlissides-bibliotheque/vlissides-bibliotheque/DTO/rechercheSimple.cs
@@ -0,0 +1,8 @@
+﻿namespace vlissides_bibliotheque.DTO
+{
+    public class RechercheSimple
+    {
+        public int numPage { get; set; }
+        public string? texteRecherche { get; set; }
+    }
+}
