d2f47eb194460a9ed8a73a7677e71051d682531e
SAMORY ROY <1855390@CegepGranby.qc.ca>
2022-11-30 14:12:48 -0500

    feat: fonction appel recherche quand changement de page

---
+++ projet/vlissides-bibliotheque/vlissides-bibliotheque/wwwroot/js/changerDePage.js
@@ -0,0 +1,51 @@
+﻿function changerPage(numPage) {
+    var conteneurBibli = document.getElementById("conteneurBibli");
+    var inputRecherche = document.getElementById("inputRecherche");
+    var texteRecherche;
+    var numeroEtudiant = "/" + window.location.pathname.replace(/^\/([^\/]*).*$/, '$1') + "/";
+    var fetchEnLocal = "/../Recherche/RechercheRapide";
+    var fetchSurServeur = numeroEtudiant + "Recherche/RechercheRapide";
+    var stringFetch = "";
+    var url = location.host;
+    var csrfToken = document.getElementsByName("__RequestVerificationToken")[0].value
+    
+
+    if (inputRecherche != null) {
+        texteRecherche=inputRecherche.value
+    }
+    if (numPage==null) {
+        numPage = 0;
+    }
+
+    if (url.match("localhost") == null) {
+        stringFetch = fetchSurServeur;
+    } else {
+        stringFetch = fetchEnLocal;
+    }
+
+    var data = {
+        numPage,
+        texteRecherche
+    }
+
+    data=JSON.stringify(data);
+
+    if (conteneurBibli != null) {
+        fetch(stringFetch, {
+            method: 'Post',
+            body: data,
+            contentType: "application/json; charset=utf-8",
+            headers: {
+                "Content-Type": "application/json",
+                //"X-CSRF-TOKEN": csrfToken
+            },
+
+        }).then(function (response) {
+            return response.text();
+
+        }).then((data) => {
+
+            conteneurBibli.innerHTML = data;
+        });
+    }
+}
