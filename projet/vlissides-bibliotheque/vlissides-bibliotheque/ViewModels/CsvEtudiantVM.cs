c5d2c1c00a64b1169bc4098910cb8b00137310c9
VINCENT CHOUINARD <1624901@CegepGranby.qc.ca>
2022-12-02 16:38:01 -0500

    feat: VM pour csv à étudiant

---
+++ projet/vlissides-bibliotheque/vlissides-bibliotheque/ViewModels/CsvEtudiantVM.cs
@@ -0,0 +1,18 @@
+﻿using CsvHelper.Configuration.Attributes;
+
+namespace vlissides_bibliotheque.ViewModels
+{
+    [Delimiter(";")]
+    public class CsvEtudiantVM
+    {
+
+        public string Matricule { get; set; }
+        public string Courriel { get; set; }
+        public string MotDePasse { get; set; }
+        public string Prenom { get; set; }
+        public string Nom { get; set; }
+        public string Adresse { get; set; }
+        public string ProgrammeEtude { get; set; }
+
+    }
+}
