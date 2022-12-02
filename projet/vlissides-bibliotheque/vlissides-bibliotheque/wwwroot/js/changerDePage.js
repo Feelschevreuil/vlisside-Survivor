bfcba5503123ab4e9c12159c8e3fca286dad9411
SAMORY ROY <1855390@CegepGranby.qc.ca>
2022-12-02 14:25:50 -0500

    feat: appel aux bonnes fonction et affichage e la recherche

--- projet/vlissides-bibliotheque/vlissides-bibliotheque/wwwroot/js/changerDePage.js
+++ projet/vlissides-bibliotheque/vlissides-bibliotheque/wwwroot/js/changerDePage.js
@@ -1,36 +1,55 @@
 ﻿function changerPage(numPage) {
     var conteneurBibli = document.getElementById("conteneurBibli");
     var inputRecherche = document.getElementById("inputRecherche");
+    var RechercheSelection = document.getElementsByClassName("RechercheBiblio");
     var texteRecherche;
+    var ouRecherche;
     var numeroEtudiant = "/" + window.location.pathname.replace(/^\/([^\/]*).*$/, '$1') + "/";
     var fetchEnLocal = "/../Recherche/RechercheRapide";
+    var fetchEnLocalDuneAutrePage = "/../Recherche/RechercheRapideAutrePage"
     var fetchSurServeur = numeroEtudiant + "Recherche/RechercheRapide";
+    var fetchSurServeurDuneAutrePage = fetchEnLocalDuneAutrePage + "Recherche/RechercheRapide";
     var stringFetch = "";
+    var stringFetchAutrePage = "";
     var url = location.host;
-    var csrfToken = document.getElementsByName("__RequestVerificationToken")[0].value
-    
+    //var csrfToken = document.getElementsByName("__RequestVerificationToken")[0].value
+
 
     if (inputRecherche != null) {
-        texteRecherche=inputRecherche.value
+        texteRecherche = inputRecherche.value
     }
-    if (numPage==null) {
+    if (numPage == null) {
         numPage = 0;
     }
 
     if (url.match("localhost") == null) {
         stringFetch = fetchSurServeur;
+        stringFetchAutrePage = fetchSurServeurDuneAutrePage;
     } else {
         stringFetch = fetchEnLocal;
+        stringFetchAutrePage = fetchEnLocalDuneAutrePage;
+    }
+
+    if (RechercheSelection == null) {
+        ouRecherche = 1; //bibli
+    } else {
+        if (RechercheSelection[1].selected) {
+            ouRecherche = 2; // etu
+        }
+        else {
+            ouRecherche = 1; // bibli
+        }
     }
 
     var data = {
         numPage,
-        texteRecherche
+        texteRecherche,
+        ouRecherche
     }
 
-    data=JSON.stringify(data);
+    data = JSON.stringify(data);
 
-    if (conteneurBibli != null) {
+    if (conteneurBibli != null) { // présentement ans la page
         fetch(stringFetch, {
             method: 'Post',
             body: data,
@@ -46,6 +65,26 @@
         }).then((data) => {
 
             conteneurBibli.innerHTML = data;
+            NbLivrePanier()
+        });
+    }
+    else {
+        fetch(stringFetchAutrePage, { // pas ans la page
+            method: 'Post',
+            body: data,
+            contentType: "application/json; charset=utf-8",
+            headers: {
+                "Content-Type": "application/json",
+                //"X-CSRF-TOKEN": csrfToken
+            },
+
+        }).then(function (response) {
+            return response.text();
+
+        }).then((data) => {
+
+            document.body.innerHTML = data;
+            NbLivrePanier()
         });
     }
 }
