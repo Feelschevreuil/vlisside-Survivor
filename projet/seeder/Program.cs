4f40fd43628304d5c4b0c45b6a23f01b0f76399b
rob <2060115@cegepgranby.qc.ca>
2022-12-04 16:38:03 -0500

    tâche: ajustement du seeder pour l'adresse de la facture

--- projet/seeder/Program.cs
+++ projet/seeder/Program.cs
@@ -925,7 +925,7 @@ namespace seeder
                 FactureEtudiantId = 0,
                 Etudiant = etudiant,
                 // TODO: ne pas enregistrer l'id de l'objet, mais l'adresse au complete en texte.
-                AdresseLivraison = "adresse place holder",
+                AdresseLivraison = etudiant.Adresse,
                 DateFacturation = DateTime
 				    .Now
 				    .AddDays(Faker.RandomNumber.Next(-355, 0)),
