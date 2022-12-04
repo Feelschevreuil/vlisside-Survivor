ba610b0548a32894f1779b8136f3d4bb050b2d44
rob <2060115@cegepgranby.qc.ca>
2022-12-04 13:34:58 -0500

    feat: service de configuration qui retourne les clés d'api

--- projet/vlissides-bibliotheque/vlissides-bibliotheque/Services/ConfigurationService.cs
+++ projet/vlissides-bibliotheque/vlissides-bibliotheque/Services/ConfigurationService.cs
@@ -1,3 +1,5 @@
+using vlissides_bibliotheque.Constantes;
+
 namespace vlissides_bibliotheque.Services
 {
 
@@ -33,6 +35,42 @@ namespace vlissides_bibliotheque.Services
                 throw new InvalidOperationException("propriété " + propriete + " de la section " + section + " non trouvée dans le fichier de configuation!");
 
             return valeur;
+        }
+
+        /// <summary>
+        /// Cherche la clé privée pour l'API de paiements.
+        /// </summary>
+        /// <returns>La clé privée pour l'API de paiements.</returns>
+        public string GetPaiementClePrivee()
+        {
+
+            string clePrive;
+
+            clePrive = GetProprieteDeSection
+            (
+                ConstantesConfiguration.PROPRIETE_STRIPE, 
+                ConstantesConfiguration.PROPRIETE_STRIPE_CLE_API_PRIVEE
+            );
+
+            return clePrive;
+        }
+
+        /// <summary>
+        /// Cherche la clé publique pour l'API de paiements.
+        /// </summary>
+        /// <returns>La clé publique pour l'API de paiements.</returns>
+        public string GetPaiementClePublique()
+        {
+
+            string clePublique;
+
+            clePublique = GetProprieteDeSection
+            (
+                ConstantesConfiguration.PROPRIETE_STRIPE, 
+                ConstantesConfiguration.PROPRIETE_STRIPE_CLE_API_PUBLIQUE
+            );
+
+            return clePublique;
         }
     }
 }
