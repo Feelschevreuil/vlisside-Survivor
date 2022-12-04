a3bd59da67f3fe102f5830c690414b6e8ffe317f
VINCENT CHOUINARD <1624901@CegepGranby.qc.ca>
2022-12-04 13:49:01 -0500

    feat: ajout de la table de liaison d'un programme d'étude à un professeur

---
+++ projet/vlissides-bibliotheque/vlissides-bibliotheque/Models/ProgrammeEtudeProfeseur.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace vlissides_bibliotheque.Models
+{
+    public class ProgrammeEtudeProfeseur
+    {
+        [Required]
+        public int ProfesseurId { get; set; }
+        public Professeur Professeur { get; set; }
+
+        [Required]
+        public int ProgrammeEtudeId { get; set; }
+        public ProgrammeEtude ProgrammeEtude { get; set; }
+    }
+}
