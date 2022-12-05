28f75545f063d93175b4448349ff00c283154891
VINCENT CHOUINARD <1624901@CegepGranby.qc.ca>
2022-12-05 00:09:12 -0500

    feat: extension pour créer les objets du document donnée-départ-projet

---
+++ projet/vlissides-bibliotheque/vlissides-bibliotheque/Extensions/boutiqueCA.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using System.Net.NetworkInformation;
+using vlissides_bibliotheque.Constantes;
+using vlissides_bibliotheque.Data;
+using vlissides_bibliotheque.Models;
+using vlissides_bibliotheque.ViewModels;
+using vlissides_bibliotheque.Enums;
+using System.Globalization;
+
+namespace vlissides_bibliotheque
+{
+    public static class boutiqueCA
+    {
+       
+
+    }
+}
+
