ad4e79c552bc16ab14ef64bbe3fe4a291dd263a0
Samuel Mongeon <2036516@cegepgranby.qc.ca>
2022-12-06 11:11:39 -0500

    feat: ajout des fichiers de base
    Création du controller et de la vue index.

---
+++ projet/vlissides-bibliotheque/vlissides-bibliotheque/Controllers/DocumentationController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace vlissides_bibliotheque.Controllers
+{
+    public class DocumentationController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
