086826693008d9f5c70a53f2c50e6a1867651d31
Samuel Mongeon <2036516@cegepgranby.qc.ca>
2022-12-06 16:52:41 -0500

    feat: ajout des fonctionnalités de base

--- projet/vlissides-bibliotheque/vlissides-bibliotheque/Controllers/DocumentationController.cs
+++ projet/vlissides-bibliotheque/vlissides-bibliotheque/Controllers/DocumentationController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using vlissides_bibliotheque.ViewModels;
 
 namespace vlissides_bibliotheque.Controllers
 {
@@ -17,7 +18,97 @@ namespace vlissides_bibliotheque.Controllers
         [HttpGet]
         public IActionResult Index()
         {
-            return View();
+            List<DocumentationVM> Fonctionnalites = new() {
+                new DocumentationVM() {
+                    Titre = "Accueil",
+                    Description = "La page d'accueil présente une vue d'ensemble des services de la plateforme.",
+                    Url = "/",
+                    Sections = new() {
+
+                    }
+                },
+                new DocumentationVM() {
+                    Titre = "Connexion",
+                    Description = "",
+                    Url = "/Connexion",
+                    Sections = new() {
+
+                    }
+                },
+                new DocumentationVM() {
+                    Titre = "Inscription",
+                    Description = "",
+                    Url = "/Connexion/Inscription",
+                    Sections = new() {
+
+                    }
+                },
+                new DocumentationVM() {
+                    Titre = "Gestion du profil",
+                    Description = "",
+                    Url = "/GestionProfil",
+                    Sections = new() {
+
+                    }
+                },
+                new DocumentationVM() {
+                    Titre = "Bibliothèque",
+                    Description = "",
+                    Url = "/Inventaire/Bibliotheque",
+                    Sections = new() {
+
+                    }
+                },
+                new DocumentationVM() {
+                    Titre = "Boutique étudiante",
+                    Description = "",
+                    Url = "/Usage/Usage",
+                    Sections = new() {
+
+                    }
+                },
+                new DocumentationVM() {
+                    Titre = "Panier",
+                    Description = "",
+                    Url = "/Achat",
+                    Sections = new() {
+
+                    }
+                },
+                new DocumentationVM() {
+                    Titre = "Événement",
+                    Description = "",
+                    Url = "/Evenement/Evenements",
+                    Sections = new() {
+
+                    }
+                },
+                new DocumentationVM() {
+                    Titre = "Recommandations",
+                    Description = "",
+                    Url = "/",
+                    Sections = new() {
+
+                    }
+                },
+                new DocumentationVM() {
+                    Titre = "Recherche de livres",
+                    Description = "",
+                    Url = "/",
+                    Sections = new() {
+
+                    }
+                },
+                new DocumentationVM() {
+                    Titre = "Tableau de bord",
+                    Description = "",
+                    Url = "/TableauDeBord",
+                    Sections = new() {
+
+                    }
+                }
+            };
+            return View(Fonctionnalites);
         }
     }
 }
