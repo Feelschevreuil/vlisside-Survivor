8d440314105feea64b9535ee1657ab5554960857
Samuel Mongeon <2036516@cegepgranby.qc.ca>
2022-12-06 16:25:47 -0500

    feat: créer/modifier/commenter les vm pour la doc

--- projet/vlissides-bibliotheque/vlissides-bibliotheque/ViewModels/DocumentationVM.cs
+++ projet/vlissides-bibliotheque/vlissides-bibliotheque/ViewModels/DocumentationVM.cs
@@ -1,6 +1,13 @@
 ﻿namespace vlissides_bibliotheque.ViewModels
 {
+    /// <summary>
+    /// Classe <c>DocumentationVM</c> offre de la documentation sur 
+    /// les fonctionnalités accessible du site web.
+    /// </summary>
     public class DocumentationVM
     {
+        public string Titre { get; set; }
+        public string Description { get; set; }
+        public List<SectionDocumentationVM> Sections { get; set; }
     }
 }
---
+++ projet/vlissides-bibliotheque/vlissides-bibliotheque/ViewModels/SectionDocumentationVM.cs
@@ -0,0 +1,12 @@
+﻿namespace vlissides_bibliotheque.ViewModels
+{
+    /// <summary>
+    /// Classe <c>SectionDocumentationVM</c> donne de l'information précise
+    /// sur une section de la documentation.
+    /// </summary>
+    public class SectionDocumentationVM
+    {
+        public string Titre { get; set; }
+        public string Description { get; set; }
+    }
+}
