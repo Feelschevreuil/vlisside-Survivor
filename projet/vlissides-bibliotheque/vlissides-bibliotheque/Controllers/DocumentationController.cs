2e59768acb4897fc721c04a31b488cb1d3ef011d
Samuel Mongeon <2036516@cegepgranby.qc.ca>
2022-12-06 21:32:43 -0500

    feat: ajout de fonctionnalité et de section

--- projet/vlissides-bibliotheque/vlissides-bibliotheque/Controllers/DocumentationController.cs
+++ projet/vlissides-bibliotheque/vlissides-bibliotheque/Controllers/DocumentationController.cs
@@ -21,15 +21,22 @@ namespace vlissides_bibliotheque.Controllers
             List<DocumentationVM> Fonctionnalites = new() {
                 new DocumentationVM() {
                     Titre = "Accueil",
-                    Description = "La page d'accueil présente une vue d'ensemble des services de la plateforme.",
+                    Description = "Présente une vue d'ensemble des services de la plateforme.",
                     Url = "/",
                     Sections = new() {
-
+                        new SectionDocumentationVM() {
+                            Titre = "Recommandations",
+                            Description = "Cette section offre un mince aperçu des livres disponibles en fonction de l'utilisateur connecté et des informations récoltés à son sujet selon ses activités sur la plateforme."
+                        },
+                        new SectionDocumentationVM() {
+                            Titre = "Événements",
+                            Description = "Cette section montre sommairement les événements disponibles, soient des activités, des commanditaires ou encore des compétitions peuvent s'y retrouver."
+                        }
                     }
                 },
                 new DocumentationVM() {
                     Titre = "Connexion",
-                    Description = "",
+                    Description = "Permet à un utilisateur déjà inscrit de se connecter à son compte.",
                     Url = "/Connexion",
                     Sections = new() {
 
@@ -37,18 +44,48 @@ namespace vlissides_bibliotheque.Controllers
                 },
                 new DocumentationVM() {
                     Titre = "Inscription",
-                    Description = "",
+                    Description = "Permet à un utilisateur qui n'est pas déjà inscrit de se créer un compte pour ensuite s'y connecter.",
                     Url = "/Connexion/Inscription",
                     Sections = new() {
-
+                        new SectionDocumentationVM() {
+                            Titre = "Accès restreint",
+                            Description = "Un étudiant ou toute autre personne ne faisant pas partie du Collège de la connaissance aléatoire ne peut malheureusment pas se créer un compte."
+                        },
+                        new SectionDocumentationVM() {
+                            Titre = "Identification",
+                            Description = ""
+                        },
+                        new SectionDocumentationVM() {
+                            Titre = "Sécurité",
+                            Description = ""
+                        },
+                        new SectionDocumentationVM() {
+                            Titre = "Scolarité",
+                            Description = ""
+                        },
+                        new SectionDocumentationVM() {
+                            Titre = "Adresse",
+                            Description = ""
+                        }
                     }
                 },
                 new DocumentationVM() {
                     Titre = "Gestion du profil",
-                    Description = "",
+                    Description = "Offre une vue d'ensemble sur les paramètres de l'utilisateur connecté. Permet aussi de mdifier ces paramètres.",
                     Url = "/GestionProfil",
                     Sections = new() {
-
+                        new SectionDocumentationVM() {
+                            Titre = "Identification",
+                            Description = ""
+                        },
+                        new SectionDocumentationVM() {
+                            Titre = "Scolarité",
+                            Description = ""
+                        },
+                        new SectionDocumentationVM() {
+                            Titre = "Adresse",
+                            Description = ""
+                        }
                     }
                 },
                 new DocumentationVM() {
@@ -56,55 +93,61 @@ namespace vlissides_bibliotheque.Controllers
                     Description = "",
                     Url = "/Inventaire/Bibliotheque",
                     Sections = new() {
-
+                        new SectionDocumentationVM() {
+                            Titre = "Format",
+                            Description = "Les livres sont offerts sous 3 formats différents au maximum, soient usagé, neuf ou numérique. Il est possible qu'un livre ne présente pas tous les formats selon les disponibilités en inventaire."
+                        },
+                        new SectionDocumentationVM() {
+                            Titre = "Ajouter",
+                            Description = "Possibilité d'ajouter un livre sous un format spécifique au panier."
+                        },
+                        new SectionDocumentationVM() {
+                            Titre = "Supprimer",
+                            Description = "Possibilité d'enlever un livre sous un format spécifique du panier."
+                        }
+                        
                     }
                 },
                 new DocumentationVM() {
                     Titre = "Boutique étudiante",
-                    Description = "",
+                    Description = "Offre la possibilité aux étudiants d'acheter des livres vendus par d'autres étudiants. Il est donc possible pour tous les étudiants de vendre ses propres livres.",
                     Url = "/Usage/Usage",
                     Sections = new() {
-
+                        new SectionDocumentationVM() {
+                            Titre = "Achat",
+                            Description = "L'achat des livres des boutiques étudiantes ne dépend pas du système de la plateforme. Ainsi, la responsabilité revient aux étudiants de spécifier les modalités pour les moyens de transaction."
+                        }
                     }
                 },
                 new DocumentationVM() {
                     Titre = "Panier",
-                    Description = "",
+                    Description = "Contient tous les livres désirés par l'utilisateur afin de lui permettre de les acheter.",
                     Url = "/Achat",
                     Sections = new() {
 
                     }
                 },
                 new DocumentationVM() {
-                    Titre = "Événement",
-                    Description = "",
+                    Titre = "Événements",
+                    Description = "Présente des actualités, des activités, des commanditaires ou encore des compétitions.",
                     Url = "/Evenement/Evenements",
                     Sections = new() {
 
                     }
                 },
                 new DocumentationVM() {
-                    Titre = "Recommandations",
-                    Description = "",
-                    Url = "/",
-                    Sections = new() {
-
-                    }
-                },
-                new DocumentationVM() {
                     Titre = "Recherche de livres",
-                    Description = "",
+                    Description = "Permet de chercher des livres de manière plus spécifique.",
                     Url = "/",
                     Sections = new() {
-
-                    }
-                },
-                new DocumentationVM() {
-                    Titre = "Tableau de bord",
-                    Description = "",
-                    Url = "/TableauDeBord",
-                    Sections = new() {
-
+                        new SectionDocumentationVM() {
+                            Titre = "Recherche simple",
+                            Description = ""
+                        },
+                        new SectionDocumentationVM() {
+                            Titre = "Recherche avancée",
+                            Description = ""
+                        }
                     }
                 }
             };
