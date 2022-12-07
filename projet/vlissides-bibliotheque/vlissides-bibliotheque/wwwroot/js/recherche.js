f157a50173c9f0032ea85805cdd6125031cd8175
SAMORY ROY <1855390@CegepGranby.qc.ca>
2022-12-07 11:04:22 -0500

    fix: selection auto sur étuiant

--- projet/vlissides-bibliotheque/vlissides-bibliotheque/wwwroot/js/recherche.js
+++ projet/vlissides-bibliotheque/vlissides-bibliotheque/wwwroot/js/recherche.js
@@ -1,1 +1,6 @@
-﻿
+﻿changerOuRechercheEtu()
+function changerOuRechercheEtu() {
+    RechercheSelection = document.getElementsByClassName("RechercheBiblio");
+    RechercheSelection[1].selected = true;
+    RechercheSelection[0].selected = false;
+}
