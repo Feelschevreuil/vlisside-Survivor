4b49908d749e830be0b19f6041f1e72781e3cce4
Samuel Mongeon <2036516@cegepgranby.qc.ca>
2022-12-07 19:12:18 -0500

    feat: ajout de js pour la taille des images doc

---
+++ projet/vlissides-bibliotheque/vlissides-bibliotheque/wwwroot/js/documentation.js
@@ -0,0 +1,8 @@
+﻿
+for (let img of document.querySelectorAll("#img-documentation")) {
+    if (img.clientWidth < 700) {
+        img.classList.add("w-50");
+    } else {
+        img.classList.add("w-100")
+    }
+}
